914a258a0a276ff22b2a25f02d6fb953fc4186e5
Andrew Head <head.andrewm@gmail.com>
2019-07-24 09:27:58 -0700

    Extension: Redirect to internal URL when visiting arXiv

---
+++ extension/launchApp.js
@@ -0,0 +1,17 @@
+/**
+ * Adapted from pdf.js pdfHandler.js
+ */
+chrome.webRequest.onHeadersReceived.addListener(
+  function(details) {
+    console.log("Trying to log header receive info");
+    return {
+      // redirectUrl: chrome.extension.getURL('extension/app.html')
+      redirectUrl: chrome.extension.getURL('build/index.html')
+    };
+  }, {
+    urls: [
+      'https://arxiv.org/pdf/*'
+    ],
+    types: [ 'main_frame', 'sub_frame' ]
+  },
+  ['blocking', 'responseHeaders']);
