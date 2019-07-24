c5c892193697659d5273391797b27ce005a1bce0
Andrew Head <head.andrewm@gmail.com>
2019-07-24 10:03:10 -0700

    Extension: MVP of react-pdf running in a Chrome extension

--- src/MyApp.js
+++ src/MyApp.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import { Document, Page, pdfjs } from 'react-pdf';
 
-pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
-
 
 class MyApp extends Component {
   state = {
