396ebb47ad0b50f82a18ffb0c19bbe1f260276f5
Andrew Head <head.andrewm@gmail.com>
2019-07-24 07:17:53 -0700

    UI: Test out basic PDF rendering component

---
+++ src/MyApp.js
@@ -0,0 +1,35 @@
+import React, { Component } from 'react';
+import { Document, Page, pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.js`;
+
+
+class MyApp extends Component {
+  state = {
+    numPages: null,
+    pageNumber: 1,
+  }
+
+  onDocumentLoadSuccess = ({ numPages }) => {
+    this.setState({ numPages });
+  }
+
+  render() {
+    const { pageNumber, numPages } = this.state;
+
+    /* `this` fix from https://github.com/wojtekmaj/react-pdf/issues/52#issuecomment-325688092 */
+    return (
+      <div>
+        <Document
+          file="https://arxiv.org/pdf/1505.05425.pdf"
+          onLoadSuccess={this.onDocumentLoadSuccess.bind(this)}
+        >
+          <Page pageNumber={pageNumber} />
+        </Document>
+        <p>Page {pageNumber} of {numPages}</p>
+      </div>
+    );
+  }
+}
+
+export default MyApp;
