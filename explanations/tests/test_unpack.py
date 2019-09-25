9c9c76217e6ec9f1484b0fcb2e167eccc338dcdd
Andrew Head <head.andrewm@gmail.com>
2019-09-25 13:17:25 -0700

    Tests: Switch to pytest from unittest

--- explanations/tests/test_unpack.py
+++ explanations/tests/test_unpack.py
@@ -1,5 +1,4 @@
 import os
-import unittest
 
 from explanations.unpack import _is_path_forbidden
 
@@ -7,19 +6,21 @@ ROOT_PATH = os.path.abspath(os.sep)
 DEST_DIR = os.path.join(ROOT_PATH, "path", "to", "dest", "dir")
 
 
-class TestUnpack(unittest.TestCase):
-    def test_allow_relative_path_in_dest_dir(self):
-        relative_inside = "filename"
-        self.assertFalse(_is_path_forbidden(relative_inside, DEST_DIR))
+def test_allow_relative_path_in_dest_dir():
+    relative_inside = "filename"
+    assert not _is_path_forbidden(relative_inside, DEST_DIR)
 
-    def test_forbid_relative_path_outside_dest_dir(self):
-        relative_outside = os.path.join(os.pardir, "filename")
-        self.assertTrue(_is_path_forbidden(relative_outside, DEST_DIR))
 
-    def test_allow_absolute_path_in_dest_dir(self):
-        absolute_outside = os.path.join(DEST_DIR, "filename")
-        self.assertFalse(_is_path_forbidden(absolute_outside, DEST_DIR))
+def test_forbid_relative_path_outside_dest_dir():
+    relative_outside = os.path.join(os.pardir, "filename")
+    assert _is_path_forbidden(relative_outside, DEST_DIR)
 
-    def test_forbid_absolute_path_outside_dest_dir(self):
-        absolute_inside = os.path.join(ROOT_PATH, "forbidden", "path")
-        self.assertTrue(_is_path_forbidden(absolute_inside, DEST_DIR))
+
+def test_allow_absolute_path_in_dest_dir():
+    absolute_outside = os.path.join(DEST_DIR, "filename")
+    assert not _is_path_forbidden(absolute_outside, DEST_DIR)
+
+
+def test_forbid_absolute_path_outside_dest_dir():
+    absolute_inside = os.path.join(ROOT_PATH, "forbidden", "path")
+    assert _is_path_forbidden(absolute_inside, DEST_DIR)
