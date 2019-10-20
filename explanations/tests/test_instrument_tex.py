d709e6abcea40d996e53d1591f9321c26f035f62
Andrew Head <head.andrewm@gmail.com>
2019-10-20 16:57:18 -0700

    Refactor: Unify how equations and tokens are colored

--- explanations/tests/test_instrument_tex.py
+++ explanations/tests/test_instrument_tex.py
@@ -3,18 +3,7 @@ import re
 from explanations.instrument_tex import colorize_equation_tokens, colorize_equations
 from explanations.types import Token
 
-EQUATION_COLOR_PATTERN = (
-    r"(.*?)"
-    + r"\\llap{"
-    + r"\\setlength{\\fboxsep}{0pt}"
-    + r"\\colorbox\[rgb\]{[0-9.]+, [0-9.]+, [0-9.]+}{"
-    + r"\\textcolor\[rgb\]{[0-9.]+, [0-9.]+, [0-9.]+}{"
-    + r"\1"
-    + r"}}}"
-)
-
-
-TOKEN_COLOR_PATTERN = (
+COLOR_PATTERN = (
     r"\\llap{"
     + r"\\pdfcolorstack0 push {[0-9.]+ [0-9.]+ [0-9.]+ rg [0-9.]+ [0-9.]+ [0-9.]+ RG}}"
     + r"(.*?)"
@@ -23,18 +12,10 @@ TOKEN_COLOR_PATTERN = (
 )
 
 
-def generate_hue_0():
-    while True:
-        yield 0
-
-
-simple_hue_generator = generate_hue_0()
-
-
 def test_color_equations():
     tex = "$eq1$ and $eq2$"
-    colorized = colorize_equations(tex, simple_hue_generator)[1]
-    matches = re.findall(EQUATION_COLOR_PATTERN, colorized)
+    colorized = colorize_equations(tex)[0]
+    matches = re.findall(COLOR_PATTERN, colorized)
     assert len(matches) == 2
     assert matches[0] == "$eq1$"
     assert matches[1] == "$eq2$"
@@ -42,9 +23,9 @@ def test_color_equations():
 
 def test_color_equation_in_argument():
     tex = "\\caption{$eq1$}"
-    colorized = colorize_equations(tex, simple_hue_generator)[1]
+    colorized = colorize_equations(tex)[0]
     assert colorized.startswith("\\caption")
-    matches = re.findall(EQUATION_COLOR_PATTERN, colorized)
+    matches = re.findall(COLOR_PATTERN, colorized)
     assert len(matches) == 1
     assert matches[0] == "$eq1$"
 
@@ -54,7 +35,7 @@ def test_color_tokens():
     tokens = {1: [Token(start=0, end=1, text="x"), Token(start=4, end=5, text="y")]}
     colorized = colorize_equation_tokens(tex, tokens)[0]
     assert colorized.startswith("$ignore$")
-    matches = re.findall(TOKEN_COLOR_PATTERN, colorized)
+    matches = re.findall(COLOR_PATTERN, colorized)
     assert len(matches) == 2
     assert matches[0] == "x"
     assert matches[1] == "y"
@@ -71,7 +52,7 @@ def test_color_subscripts():
         ]
     }
     colorized = colorize_equation_tokens(tex, tokens)[0]
-    matches = re.findall(TOKEN_COLOR_PATTERN, colorized)
+    matches = re.findall(COLOR_PATTERN, colorized)
     assert len(matches) == 4
     # Subscript and superscript commands must also be wrapped in the coloring commands
     assert matches[0] == "_i"
