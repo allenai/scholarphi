95b662d406229f541f5b5ea901acd7726c6b11b0
Andrew Head <head.andrewm@gmail.com>
2019-10-24 23:50:50 -0700

    API: Connect to Postgres database with Knex

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,26 @@
 import * as Hapi from "@hapi/hapi";
 import * as Joi from "@hapi/joi";
+import * as knex_init from "knex";
+import * as nconf from "nconf";
 
-export const init = async () => {
+export const init = async (config: nconf.Provider) => {
   const server = new Hapi.Server({
     port: 3000,
-    host: "localhost"
+    host: "localhost",
+    debug: {
+      request: ["error"]
+    }
+  });
+
+  const knex = knex_init({
+    client: "pg",
+    connection: {
+      host: config.get("database:host"),
+      port: config.get("database:port"),
+      user: config.get("database:user"),
+      password: config.get("database:password"),
+      database: config.get("database:database")
+    }
   });
 
   server.route({
@@ -12,7 +28,9 @@ export const init = async () => {
     path: "/{s2id}/citations",
     handler: request => {
       const s2id = request.params.s2id;
-      return { key: "value", another_key: 2, s2id };
+      return knex("citation")
+        .select()
+        .first();
     },
     options: {
       validate: {
@@ -27,6 +45,12 @@ export const init = async () => {
 
   await server.start();
   console.log("Server running on %s", server.info.uri);
+
+  server.events.on("log", (event, tags) => {
+    if (tags.error) {
+      console.log(`Server error: ${event.error ? event.error : "unknown"}`);
+    }
+  });
 };
 
 process.on("unhandledRejection", err => {
