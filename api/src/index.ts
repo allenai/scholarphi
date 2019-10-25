090f9201e2888a2dbeb217614271d03fdd6ae376
Andrew Head <head.andrewm@gmail.com>
2019-10-24 23:50:50 -0700

    API: Connect to Postgres database with Knex

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,12 +9,9 @@ nconf
     database: {
       host: "scholar-reader.cjc2varstph5.us-east-2.rds.amazonaws.com",
       port: 5432,
-      name: "postgres",
-      username: "postgres"
+      database: "postgres",
+      user: "postgres"
     }
   });
 
-console.log(nconf.get("database:name"));
-console.log(nconf.get("database:other"));
-
-init();
+init(nconf);
