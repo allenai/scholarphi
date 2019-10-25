6e36febd5ee6fdba99dfbf4a61f694bcd63f1b63
Andrew Head <head.andrewm@gmail.com>
2019-10-24 22:23:58 -0700

    API: Added simple endpoint that returns JSON and validates parameters

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,9 +1,28 @@
 import * as Hapi from "@hapi/hapi";
+import * as Joi from "@hapi/joi";
 
 const init = async () => {
   const server = new Hapi.Server({
     port: 3000,
     host: "localhost"
+  });
+
+  server.route({
+    method: "GET",
+    path: "/{s2id}/citations",
+    handler: request => {
+      const s2id = request.params.s2id;
+      return { key: "value", another_key: 2, s2id };
+    },
+    options: {
+      validate: {
+        params: Joi.object({
+          s2id: Joi.string()
+            .alphanum()
+            .length(40)
+        })
+      }
+    }
   });
 
   await server.start();
