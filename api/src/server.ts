76e2174404091a29b4c9a6c1da3333fb1c0c3ae5
Andrew Head <head.andrewm@gmail.com>
2019-10-26 13:34:07 -0700

    API: Return citations by arXiv ID too

--- api/src/server.ts
+++ api/src/server.ts
@@ -16,17 +16,46 @@ export const init = async (config: nconf.Provider) => {
 
   server.route({
     method: "GET",
-    path: "/{s2id}/citations",
+    path: "/{s2Id}/citations",
     handler: request => {
-      const s2id = request.params.s2id;
-      return dbConnection.getCitations(s2id);
+      const s2Id = request.params.s2Id;
+      return dbConnection.getCitationsForS2Id(s2Id);
     },
     options: {
       validate: {
         params: Joi.object({
-          s2id: Joi.string()
+          s2Id: Joi.string()
             .alphanum()
             .length(40)
+        })
+      }
+    }
+  });
+
+  server.route({
+    method: "GET",
+    path: "/arxiv:{arxivId}/citations",
+    handler: request => {
+      const arxivId = request.params.arxivId;
+      return dbConnection.getCitationsForArxivId(arxivId);
+    },
+    options: {
+      validate: {
+        params: Joi.object({
+          /*
+           * See the arXiv documentation on valid identifiers here:
+           * https://arxiv.org/help/arxiv_identifier.
+           */
+          arxivId: Joi.alternatives().try(
+            /*
+             * Current arXiv ID format.
+             */
+            Joi.string().pattern(/[0-9]{2}[0-9]{2}.[0-9]+(v[0-9]+)?/),
+            /*
+             * Older arXiv ID format.
+             */
+            Joi.string().pattern(/[a-zA-Z0-9-]+\.[A-Z]{2}\/[0-9]{2}[0-9]{2}[0-9]+(v[0-9]+)/)
+          )
         })
       }
     }
