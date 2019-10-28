e9594663d24707aa315ecb087c7ff2fca0325bf6
Andrew Head <head.andrewm@gmail.com>
2019-10-28 16:52:07 -0700

    UI: Fetch paper abstracts from API

--- ui/src/api.ts
+++ ui/src/api.ts
@@ -1,13 +1,30 @@
 import axios from "axios";
-import { Citation } from "./types/api";
+import { Citation, Paper } from "./types/api";
 
 export async function citationsForArxivId(arxivId: string) {
   try {
-    const response = await axios.get(`/api/v0/arxiv:${arxivId}/citations`);
+    const response = await axios.get(`/api/v0/papers/arxiv:${arxivId}/citations`);
     if (response.status === 200) {
       return response.data as Citation[];
     }
-    throw Error("Unexpected return value");
+    throw Error(`API Error: Unexpected response ${response}`);
+  } catch (error) {
+    console.error("API Error:", error);
+  }
+  return [];
+}
+
+export async function papers(s2Ids: string[]) {
+  try {
+    const response = await axios.get(`/api/v0/papers`, {
+      params: {
+        id: s2Ids.join(",")
+      }
+    });
+    if (response.status === 200) {
+      return response.data as Paper[];
+    }
+    throw Error(`API Error: Unexpected response ${response}`);
   } catch (error) {
     console.error("API Error:", error);
   }
--- ui/src/selectors/citation.ts
+++ ui/src/selectors/citation.ts
@@ -6,6 +6,6 @@ import { BoundingBox, Citation } from "../types/api";
 export function citationKey(bounding_box: BoundingBox, citation: Citation) {
   return (
     `${bounding_box.page}-L${bounding_box.left}-T${bounding_box.top}-W${bounding_box.width}-H${bounding_box.height} ` +
-    `${citation.papers.map(p => p.s2Id).join("-")}`
+    `${citation.papers.join("-")}`
   );
 }
