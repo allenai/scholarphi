d86e9cd932a9ffabd5612adc09581289c8d4f38e
Andrew Head <head.andrewm@gmail.com>
2019-10-30 15:06:28 -0700

    Refactor: Move annotation calculation into a selector

--- ui/src/CitationAnnotation.tsx
+++ ui/src/CitationAnnotation.tsx
@@ -1,6 +1,7 @@
 import Tooltip from "@material-ui/core/Tooltip";
 import React from "react";
 import CitationTooltipBody from "./CitationTooltipBody";
+import * as selectors from "./selectors";
 import { BoundingBox, Citation } from "./types/api";
 import { PDFPageView } from "./types/pdfjs-viewer";
 
@@ -18,31 +19,6 @@ interface CitationAnnotationProps {
 
 export class CitationAnnotation extends React.Component<CitationAnnotationProps, {}> {
   render() {
-    /**
-     * XXX(andrewhead): A slight scale correction was needed to make the bounding boxes appear in
-     * just the right place on a test PDF. Maybe this reflects some round-off error in bounding box
-     * location detection in the data processing scripts?
-     */
-    const SCALE_CORRECTION = 0.9975;
-    const viewport = this.props.pageView.pdfPage.getViewport({
-      scale: this.props.pageView.viewport.scale * SCALE_CORRECTION
-    });
-
-    const box = this.props.location;
-    const pdfCoordinates = [box.left, box.top, box.left + box.width, box.top + box.height];
-    const viewportBox = viewport.convertToViewportRectangle(pdfCoordinates);
-
-    /**
-     * Based on how pdf-react transforms PDF coordinates to viewport coordinates here:
-     * https://github.com/wojtekmaj/react-pdf/blob/73f505eca1bf1ae243a2b7068fce1e86b98b408a/src/Page/AnnotationLayer.jsx#L104
-     */
-    const dimensionStyles = {
-      left: viewportBox[0],
-      top: viewportBox[1],
-      width: viewportBox[2] - viewportBox[0],
-      height: viewportBox[1] - viewportBox[3]
-    };
-
     return (
       <Tooltip
         interactive
@@ -50,7 +26,10 @@ export class CitationAnnotation extends React.Component<CitationAnnotationProps,
         className="citation-tooltip"
         title={<CitationTooltipBody paperIds={this.props.citation.papers} />}
       >
-        <div className="citation-annotation" style={dimensionStyles} />
+        <div
+          className="citation-annotation"
+          style={selectors.divDimensionStyles(this.props.pageView, this.props.location)}
+        />
       </Tooltip>
     );
   }
