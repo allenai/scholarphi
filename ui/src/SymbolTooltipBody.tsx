f1a203b1f85a87701efda9fc86e09a45391ac4a5
Andrew Head <head.andrewm@gmail.com>
2019-11-04 18:12:58 -0800

    UI: Carousel for reviewing all other appearances of a symbol

--- ui/src/SymbolTooltipBody.tsx
+++ ui/src/SymbolTooltipBody.tsx
@@ -1,15 +1,21 @@
 import Button from "@material-ui/core/Button";
 import React from "react";
 import { PaperClipping } from "./PaperClipping";
+import * as selectors from "./selectors";
 import { ScholarReaderContext } from "./state";
-import { BoundingBox, Symbol } from "./types/api";
+import { Symbol } from "./types/api";
 import { PDFViewer } from "./types/pdfjs-viewer";
 
 interface SymbolTooltipBodyProps {
   symbol: Symbol;
 }
 
-function scrollToBox(pdfViewer: PDFViewer, box: BoundingBox) {
+interface SymbolTooltipBodyState {
+  activeSymbol?: Symbol;
+  activeSymbolIndex: number;
+}
+
+function scrollToSymbol(pdfViewer: PDFViewer, symbol: Symbol) {
   /*
    * Based roughly on the scroll offsets used for pdf.js "find" functionality:
    * https://github.com/mozilla/pdf.js/blob/16ae7c6960c1296370c1600312f283a68e82b137/web/pdf_find_controller.js#L190-L191
@@ -18,32 +24,82 @@ function scrollToBox(pdfViewer: PDFViewer, box: BoundingBox) {
   const SCROLL_OFFSET_X = -400;
   const SCROLL_OFFSET_Y = +100;
 
+  const box = symbol.bounding_boxes[0];
   pdfViewer.scrollPageIntoView({
     pageNumber: box.page + 1,
     destArray: [undefined, { name: "XYZ" }, box.left + SCROLL_OFFSET_X, box.top + SCROLL_OFFSET_Y]
   });
 }
 
-export class SymbolTooltipBody extends React.Component<SymbolTooltipBodyProps, {}> {
+export class SymbolTooltipBody extends React.Component<
+  SymbolTooltipBodyProps,
+  SymbolTooltipBodyState
+> {
+  static contextType = ScholarReaderContext;
+  context!: React.ContextType<typeof ScholarReaderContext>;
+
+  constructor(props: SymbolTooltipBodyProps) {
+    super(props);
+    this.state = {
+      activeSymbolIndex: 0
+    };
+  }
+
+  previousSymbol(symbols: Symbol[]) {
+    const activeSymbolIndex = (this.state.activeSymbolIndex - 1) % symbols.length;
+    this.setState({
+      activeSymbolIndex,
+      activeSymbol: symbols[activeSymbolIndex]
+    });
+  }
+
+  nextSymbol(symbols: Symbol[]) {
+    const activeSymbolIndex = (this.state.activeSymbolIndex + 1) % symbols.length;
+    this.setState({
+      activeSymbolIndex,
+      activeSymbol: symbols[activeSymbolIndex]
+    });
+  }
+
   render() {
-    const firstBoundingBox = this.props.symbol.bounding_boxes[0];
+    const matchingSymbols = selectors.getMatchingSymbols(
+      [...this.context.symbols],
+      this.props.symbol
+    );
+
     return (
       <div className="symbol-tooltip-body">
-        <PaperClipping
-          pageNumber={firstBoundingBox.page + 1}
-          highlightBoxes={this.props.symbol.bounding_boxes}
-        />
+        {matchingSymbols.map((symbol, i) => {
+          const { bounding_boxes } = symbol;
+          const hidden = i !== this.state.activeSymbolIndex;
+          return (
+            <div key={selectors.symbolKey(symbol, bounding_boxes[0])} hidden={hidden}>
+              {/* Only render if the symbol is not hidden. */}
+              {hidden === false && (
+                <PaperClipping
+                  pageNumber={bounding_boxes[0].page + 1}
+                  highlightBoxes={bounding_boxes}
+                />
+              )}
+            </div>
+          );
+        })}
+        <Button onClick={() => this.previousSymbol(matchingSymbols)}>Previous</Button>
         <ScholarReaderContext.Consumer>
           {({ pdfViewer }) => (
             <Button
               onClick={() => {
-                pdfViewer !== null && scrollToBox(pdfViewer, firstBoundingBox);
+                const { activeSymbol } = this.state;
+                if (activeSymbol !== undefined && pdfViewer !== null) {
+                  scrollToSymbol(pdfViewer, activeSymbol);
+                }
               }}
             >
               Jump to this location
             </Button>
           )}
         </ScholarReaderContext.Consumer>
+        <Button onClick={() => this.nextSymbol(matchingSymbols)}>Next</Button>
       </div>
     );
   }
--- ui/src/selectors/symbol.ts
+++ ui/src/selectors/symbol.ts
@@ -4,3 +4,11 @@ import { boundingBoxString } from "./annotation";
 export function symbolKey(symbol: Symbol, location: BoundingBox) {
   return `${symbol.tex} ${boundingBoxString(location)}`;
 }
+
+/**
+ * Get a list of symbols that match a provided 'symbol'. Currently, this performs a text match.
+ * In the future, this might have more nuanced matching behavior.
+ */
+export function getMatchingSymbols(symbols: Symbol[], symbol: Symbol) {
+  return symbols.filter(other => other.tex === symbol.tex);
+}
