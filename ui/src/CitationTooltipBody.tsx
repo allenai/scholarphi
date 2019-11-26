6591ecaf7384a809521e2d108e95a4f87785f088
Andrew Head <head.andrewm@gmail.com>
2019-11-25 20:51:24 -0800

    UI: Change citation tooltip to show clickable list of titles

--- ui/src/CitationTooltipBody.tsx
+++ ui/src/CitationTooltipBody.tsx
@@ -1,91 +1,31 @@
-import Button from "@material-ui/core/Button";
-import MobileStepper from "@material-ui/core/MobileStepper";
-import KeyboardArrowLeft from "@material-ui/icons/KeyboardArrowLeft";
-import KeyboardArrowRight from "@material-ui/icons/KeyboardArrowRight";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
 import React from "react";
-import PaperSummary from "./PaperSummary";
-import * as selectors from "./selectors";
-import { ScholarReaderContext } from "./state";
+import Citation from "./Citation";
 
 interface CitationTooltipBodyProps {
   paperIds: string[];
 }
 
-interface CitationTooltipBodyState {
-  activeCitation: number;
-}
-
 export class CitationTooltipBody extends React.Component<
-  CitationTooltipBodyProps,
-  CitationTooltipBodyState
+  CitationTooltipBodyProps
 > {
-  constructor(props: CitationTooltipBodyProps) {
-    super(props);
-    this.state = {
-      activeCitation: 0
-    };
-  }
-
   render() {
     return (
       <div className="tooltip-body citation-tooltip-body">
-        {this.props.paperIds.map((paperId, i) => (
-          <div
-            className="citation-tooltip-body__paper-summary-container"
-            key={paperId}
-            hidden={i !== this.state.activeCitation}
-          >
-            <PaperSummary key={paperId} paperId={paperId} />
-          </div>
-        ))}
-        <ScholarReaderContext.Consumer>
-          {({ papers }) => (
-            <div className="citation-tooltip-body__stepper-container">
-              <MobileStepper
-                hidden={
-                  selectors.countOfCitationsWithSummaries(
-                    papers,
-                    this.props.paperIds
-                  ) <= 1
-                }
-                variant="dots"
-                steps={this.props.paperIds.length}
-                position="static"
-                activeStep={this.state.activeCitation}
-                nextButton={
-                  <Button
-                    size="small"
-                    onClick={() =>
-                      this.setState({
-                        activeCitation: this.state.activeCitation + 1
-                      })
-                    }
-                    disabled={
-                      this.state.activeCitation ===
-                      this.props.paperIds.length - 1
-                    }
-                  >
-                    Next <KeyboardArrowRight />
-                  </Button>
-                }
-                backButton={
-                  <Button
-                    size="small"
-                    onClick={() =>
-                      this.setState({
-                        activeCitation: this.state.activeCitation - 1
-                      })
-                    }
-                    disabled={this.state.activeCitation === 0}
-                  >
-                    <KeyboardArrowLeft />
-                    Back
-                  </Button>
-                }
-              />
-            </div>
-          )}
-        </ScholarReaderContext.Consumer>
+        <div className="tooltip-body__section tooltip-body__label citation-tooltip-body__header">
+          This citation was matched to {this.props.paperIds.length}
+          {this.props.paperIds.length > 1 ? " papers" : " paper"}:
+        </div>
+        <div className="tooltip-body__section">
+          <List aria-label="cited papers">
+            {this.props.paperIds.map((paperId, i) => (
+              <ListItem disableGutters key={paperId} button onClick={() => {}}>
+                <Citation key={paperId} paperId={paperId} />
+              </ListItem>
+            ))}
+          </List>
+        </div>
       </div>
     );
   }
