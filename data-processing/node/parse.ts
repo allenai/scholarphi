3d8ec633e669a6000076879b53e290e86996f489
Andrew Head <head.andrewm@gmail.com>
2019-12-12 14:30:26 -0800

    TeX: Add flags for parsing equations with or without throwing KaTeX parse errors

--- data-processing/node/parse.ts
+++ data-processing/node/parse.ts
@@ -6,16 +6,18 @@ import { EquationParseResult } from "./types";
 
 program
   .command("equations-csv <csv-file>")
+  .option("-t, --throw-on-error", "Make KaTeX throw a parse error when it fails to parse an equation.")
+  .option("-c, --error-color <color>", "Hex code for color of parse error nodes in MathML.")
   .description(
     "Parse equations from CSV file. Expected format of each row is [path], [i], [equation-tex]"
   )
-  .action(csvPath => {
+  .action((csvPath, cmdObj) => {
     csv.parseFile(csvPath).on("data", row => {
       const [path, i] = row;
-      const equation = row[7];
+      const equation = row[11];
       const baseResult = { i, path, equation };
       let result: EquationParseResult;
-      if (row.length < 8) {
+      if (row.length < 12) {
         result = {
           ...baseResult,
           success: false,
@@ -25,7 +27,7 @@ program
         };
       } else {
         try {
-          const mathMl = parse(equation);
+          const mathMl = parse(equation, cmdObj.throwOnError, cmdObj.errorColor);
           result = { ...baseResult, success: true, mathMl, errorMessage: null };
         } catch (e) {
           result = {
@@ -42,11 +44,13 @@ program
 
 program
   .command("equation <equation>")
+  .option("-t, --throw-on-error", "Make KaTeX throw a parse error when it fails to parse an equation.")
+  .option("-c, --error-color <color>", "Hex code for color of parse error nodes in MathML.")
   .description(
     "Parse a TeX equation. Provide equation as a string, without surrounding delimiters (e.g., '$', '\\begin{equation}', etc."
   )
-  .action(equation => {
-    console.log(parse(equation));
+  .action((equation, cmdObj) => {
+    console.log(parse(equation, cmdObj.throwOnError, cmdObj.errorColor));
   });
 
 program.parse(process.argv);
@@ -54,9 +58,10 @@ program.parse(process.argv);
 /**
  * Parse an equation into a MathML tree with source location annotations.
  */
-function parse(equation: string): string {
+function parse(equation: string, throwOnError?: boolean, errorColor?: string): string {
   return katex.renderToString(equation, {
     output: "mathml",
-    throwOnError: true
+    throwOnError: throwOnError || false,
+    errorColor
   });
 }
--- data-processing/tests/test_parse_equation.py
+++ data-processing/tests/test_parse_equation.py
@@ -14,6 +14,14 @@ def test_get_characters():
         assert Character("y", 2, 4, 5) in characters
 
 
+def test_get_characters_omits_error_nodes():
+    with open(get_test_path(os.path.join("mathml", "x_plus_error.xml"))) as mathml_file:
+        mathml = mathml_file.read()
+        characters = get_characters(mathml)
+        assert len(characters) == 1
+        assert Character("x", 0, 0, 1) in characters
+
+
 def test_get_symbols_for_characters():
     with open(get_test_path(os.path.join("mathml", "x_plus_y.xml"))) as mathml_file:
         mathml = mathml_file.read()
@@ -26,6 +34,17 @@ def test_get_symbols_for_characters():
 
     y = list(filter(lambda s: s.characters == [2], symbols))[0]
     assert y.mathml.strip() == "<mi>y</mi>"
+
+
+def test_get_symbols_omits_error_nodes():
+    with open(get_test_path(os.path.join("mathml", "x_plus_error.xml"))) as mathml_file:
+        mathml = mathml_file.read()
+        symbols = get_symbols(mathml)
+
+    assert len(symbols) == 1
+
+    x = list(filter(lambda s: s.characters == [0], symbols))[0]
+    assert x.mathml.strip() == "<mi>x</mi>"
 
 
 def test_get_symbols_for_subscript():
@@ -41,7 +60,7 @@ def test_get_symbols_for_subscript():
     assert 1 in x_sub_i.characters
 
 
-def test_get_symboL_children():
+def test_get_symbol_children():
     with open(
         get_test_path(os.path.join("mathml", "x_sub_t_sub_i.xml"))
     ) as mathml_file:
