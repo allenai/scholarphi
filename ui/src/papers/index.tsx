20a2d017f5c2d6e92111cb1519eaeb102d781980
Andrew Head <head.andrewm@gmail.com>
2019-12-20 08:46:59 -0800

    UI: Whitelist a smaller number of papers for the demo

--- ui/src/papers/index.tsx
+++ ui/src/papers/index.tsx
@@ -8,65 +8,65 @@
  * the general reader interface slightly.
  */
 
-import ReactDOM from 'react-dom';
-import React, { useState, useEffect } from 'react';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import ErrorIcon from '@material-ui/icons/Error';
-import Grid from '@material-ui/core/Grid';
-import Box from '@material-ui/core/Box';
-import Typography from '@material-ui/core/Typography';
-import CircularProgress from '@material-ui/core/CircularProgress';
-import axios from 'axios';
-
-import { getAllPapers } from '../api';
-import { PaperIdWithCounts } from '../types/api';
+import AppBar from "@material-ui/core/AppBar";
+import Box from "@material-ui/core/Box";
+import CircularProgress from "@material-ui/core/CircularProgress";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import Grid from "@material-ui/core/Grid";
+import Table from "@material-ui/core/Table";
+import TableBody from "@material-ui/core/TableBody";
+import TableCell from "@material-ui/core/TableCell";
+import TableHead from "@material-ui/core/TableHead";
+import TableRow from "@material-ui/core/TableRow";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import ErrorIcon from "@material-ui/icons/Error";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import ReactDOM from "react-dom";
+import { getAllPapers } from "../api";
+import { PaperIdWithCounts } from "../types/api";
 
 enum ViewState {
-    LOADING, READY, ERROR
+  LOADING,
+  READY,
+  ERROR
 }
 
 interface PaperListState {
-    state: ViewState;
-    papers: PaperWithMeta[];
+  state: ViewState;
+  papers: PaperWithMeta[];
 }
 
-
 export interface S2ApiPaper {
-    abstract: string;
-    arxivId?: string;
-    authors: S2ApiAuthor[];
-    doi: string;
-    title: string;
-    url: string;
-    venue?: string;
-    year: string;
+  abstract: string;
+  arxivId?: string;
+  authors: S2ApiAuthor[];
+  doi: string;
+  title: string;
+  url: string;
+  venue?: string;
+  year: string;
 }
 
 interface S2ApiAuthor {
-    authorId: string;
-    name: string;
-    url: string;
+  authorId: string;
+  name: string;
+  url: string;
 }
 
 // We only retain the fields from S2ApiPaper that we need, as otherwise
 // we can very easily overflow localStorage.
 interface PaperWithMeta extends PaperIdWithCounts {
-    title: string;
-    authors: S2ApiAuthor[];
-    venue?: string;
-    year: string;
+  title: string;
+  authors: S2ApiAuthor[];
+  venue?: string;
+  year: string;
 }
 
 interface CachedPaperWithMeta {
-    expires: number;
-    paper: PaperWithMeta;
+  expires: number;
+  paper: PaperWithMeta;
 }
 
 /**
@@ -76,135 +76,161 @@ interface CachedPaperWithMeta {
  * very basic mechanism and should likely be replaced with something in the
  * reader API in the future.
  */
-async function getPaperInfoFromS2(paper: PaperIdWithCounts): Promise<PaperWithMeta> {
-    if (localStorage) {
-        const maybeItem = localStorage.getItem(paper.s2Id);
-        if (maybeItem) {
-            const parsedItem: CachedPaperWithMeta = JSON.parse(maybeItem);
-            if (parsedItem.expires > Date.now()) {
-                console.debug(`Using cached paper:${paper.s2Id}`);
-                return Promise.resolve(parsedItem.paper);
-            } else {
-                console.debug(`Paper ${paper.s2Id} is cached but has expired.`);
-            }
-        }
+async function getPaperInfoFromS2(
+  paper: PaperIdWithCounts
+): Promise<PaperWithMeta> {
+  if (localStorage) {
+    const maybeItem = localStorage.getItem(paper.s2Id);
+    if (maybeItem) {
+      const parsedItem: CachedPaperWithMeta = JSON.parse(maybeItem);
+      if (parsedItem.expires > Date.now()) {
+        console.debug(`Using cached paper:${paper.s2Id}`);
+        return Promise.resolve(parsedItem.paper);
+      } else {
+        console.debug(`Paper ${paper.s2Id} is cached but has expired.`);
+      }
     }
-    const s2ApiUrl =`https://api.semanticscholar.org/v1/paper/${paper.s2Id}`;
-    const { data: s2Paper } = await axios.get<S2ApiPaper>(s2ApiUrl);
-    const paperWithMeta = Object.assign(paper, {
-        title: s2Paper.title,
-        authors: s2Paper.authors,
-        venue: s2Paper.venue,
-        year: s2Paper.year
-    });
-    if (localStorage) {
-        localStorage.setItem(paper.s2Id, JSON.stringify({
-            // cache for 24 hours, JavaScript timestamps are expressed in ms
-            expires: Date.now() + (24 * 60 * 60 * 1000),
-            paper: paperWithMeta
-        }))
-    }
-    return paperWithMeta;
+  }
+  const s2ApiUrl = `https://api.semanticscholar.org/v1/paper/${paper.s2Id}`;
+  const { data: s2Paper } = await axios.get<S2ApiPaper>(s2ApiUrl);
+  const paperWithMeta = Object.assign(paper, {
+    title: s2Paper.title,
+    authors: s2Paper.authors,
+    venue: s2Paper.venue,
+    year: s2Paper.year
+  });
+  if (localStorage) {
+    localStorage.setItem(
+      paper.s2Id,
+      JSON.stringify({
+        // cache for 24 hours, JavaScript timestamps are expressed in ms
+        expires: Date.now() + 24 * 60 * 60 * 1000,
+        paper: paperWithMeta
+      })
+    );
+  }
+  return paperWithMeta;
 }
 
 const PaperList = () => {
-    const [ { state, papers }, setViewState ] = useState<PaperListState>({
-        papers: [], state: ViewState.LOADING
-    });
-    useEffect(() => {
-        (async () => {
-            const allPapers = await getAllPapers();
-            if (allPapers === null) {
-                setViewState({ papers: [], state: ViewState.ERROR });
-                return;
-            }
-            const allPapersWithTitle =
-                await Promise.all(allPapers.map(getPaperInfoFromS2));
-            setViewState({
-                papers: allPapersWithTitle,
-                state: ViewState.READY
-            });
-        })();
-        return () => setViewState({ papers: [], state: ViewState.LOADING });
-    }, []); // The empty array makes the inner callback only execute once
-    return (
-        <>
-            <CssBaseline />
-            <AppBar position="sticky">
-                <Toolbar>
-                    <h1>Supported Papers</h1>
-                </Toolbar>
-            </AppBar>
-            <div>
-                {state === ViewState.LOADING && (
-                    <Box p={4} textAlign="center">
-                        <CircularProgress />
-                    </Box>
-                )}
-                {state === ViewState.READY && (
-                    <Table>
-                        <TableHead>
-                            <TableRow>
-                                <TableCell>Paper</TableCell>
-                                <TableCell>ArXiv ID</TableCell>
-                                <TableCell>Symbols</TableCell>
-                                <TableCell>Citations</TableCell>
-                                <TableCell>Reader Link</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
-                            {papers.map(paper => (
-                                <TableRow key={paper.s2Id}>
-                                    <TableCell>
-                                        <Typography variant="subtitle1">
-                                            <strong>
-                                                <a href={`https://semanticscholar.org/paper/${paper.s2Id}`}>
-                                                    {paper.title || paper.s2Id}
-                                                </a>
-                                            </strong>
-                                        </Typography>
-                                        {paper.authors.map((author, idx) => (
-                                            <React.Fragment key={author.url}>
-                                                <a href={author.url}>
-                                                    {author.name}
-                                                </a>
-                                                {idx !== paper.authors.length - 1 ? ', ' : null}
-                                            </React.Fragment>
-                                        ))}
-                                        {paper.venue && ` • ${paper.venue}`}
-                                        {" •"} {paper.year}
-                                    </TableCell>
-                                    <TableCell>{paper.arxivId
-                                        ? <a href={`https://arxiv.org/abs/${paper.arxivId}`}>
-                                            {paper.arxivId}
-                                          </a>
-                                        : null
-                                    }</TableCell>
-                                    <TableCell>{paper.extractedSymbolCount}</TableCell>
-                                    <TableCell>{paper.extractedCitationCount}</TableCell>
-                                    <TableCell>
-                                        {paper.arxivId
-                                            ? <a href={`/?file=https://arxiv.org/pdf/${paper.arxivId}.pdf`}>
-                                                Read
-                                              </a>
-                                            : null}
-                                    </TableCell>
-                                </TableRow>
-                            ))}
-                        </TableBody>
-                    </Table>
-                )}
-                {state === ViewState.ERROR && (
-                    <Box p={2}>
-                        <Grid container alignItems="center" spacing={1}>
-                            <Grid item><ErrorIcon /></Grid>
-                            <Grid item>Shucks, something went wrong.</Grid>
-                        </Grid>
-                    </Box>
-                )}
-            </div>
-        </>
-    );
-}
+  const [{ state, papers }, setViewState] = useState<PaperListState>({
+    papers: [],
+    state: ViewState.LOADING
+  });
+  useEffect(() => {
+    (async () => {
+      const allPapers = await getAllPapers();
+      if (allPapers === null) {
+        setViewState({ papers: [], state: ViewState.ERROR });
+        return;
+      }
+      const allPapersWithTitle = await Promise.all(
+        allPapers.map(getPaperInfoFromS2)
+      );
+      setViewState({
+        papers: allPapersWithTitle,
+        state: ViewState.READY
+      });
+    })();
+    return () => setViewState({ papers: [], state: ViewState.LOADING });
+  }, []); // The empty array makes the inner callback only execute once
+  return (
+    <>
+      <CssBaseline />
+      <AppBar position="sticky">
+        <Toolbar>
+          <h1>Supported Papers</h1>
+        </Toolbar>
+      </AppBar>
+      <div>
+        {state === ViewState.LOADING && (
+          <Box p={4} textAlign="center">
+            <CircularProgress />
+          </Box>
+        )}
+        {state === ViewState.READY && (
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Paper</TableCell>
+                <TableCell>ArXiv ID</TableCell>
+                <TableCell>Symbols</TableCell>
+                <TableCell>Citations</TableCell>
+                <TableCell>Reader Link</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {papers
+                .filter(paper => {
+                  return (
+                    paper.arxivId !== undefined &&
+                    [
+                      "1508.07252",
+                      "1706.03762",
+                      "1811.12160",
+                      "1010.2823",
+                      "1309.7440",
+                      "1404.6635"
+                    ].indexOf(paper.arxivId) !== -1
+                  );
+                })
+                .map(paper => (
+                  <TableRow key={paper.s2Id}>
+                    <TableCell>
+                      <Typography variant="subtitle1">
+                        <strong>
+                          <a
+                            href={`https://semanticscholar.org/paper/${paper.s2Id}`}
+                          >
+                            {paper.title || paper.s2Id}
+                          </a>
+                        </strong>
+                      </Typography>
+                      {paper.authors.map((author, idx) => (
+                        <React.Fragment key={author.url}>
+                          <a href={author.url}>{author.name}</a>
+                          {idx !== paper.authors.length - 1 ? ", " : null}
+                        </React.Fragment>
+                      ))}
+                      {paper.venue && ` • ${paper.venue}`}
+                      {" •"} {paper.year}
+                    </TableCell>
+                    <TableCell>
+                      {paper.arxivId ? (
+                        <a href={`https://arxiv.org/abs/${paper.arxivId}`}>
+                          {paper.arxivId}
+                        </a>
+                      ) : null}
+                    </TableCell>
+                    <TableCell>{paper.extractedSymbolCount}</TableCell>
+                    <TableCell>{paper.extractedCitationCount}</TableCell>
+                    <TableCell>
+                      {paper.arxivId ? (
+                        <a
+                          href={`/?file=https://arxiv.org/pdf/${paper.arxivId}.pdf`}
+                        >
+                          Read
+                        </a>
+                      ) : null}
+                    </TableCell>
+                  </TableRow>
+                ))}
+            </TableBody>
+          </Table>
+        )}
+        {state === ViewState.ERROR && (
+          <Box p={2}>
+            <Grid container alignItems="center" spacing={1}>
+              <Grid item>
+                <ErrorIcon />
+              </Grid>
+              <Grid item>Shucks, something went wrong.</Grid>
+            </Grid>
+          </Box>
+        )}
+      </div>
+    </>
+  );
+};
 
-ReactDOM.render(<PaperList />, document.getElementById('root'));
+ReactDOM.render(<PaperList />, document.getElementById("root"));
