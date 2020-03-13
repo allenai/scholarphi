cdb37141262f2474970c52bf9f08137d18a75960
Zachary Kirby <zkirby@berkeley.edu>
2020-03-13 10:15:43 -0700

    Merge Consecutive <mi> Tags (#63)
    * initial code
    
    * updates to testing
    
    * updates
    
    * udpates to comments
    
    * Update data-processing/common/parse_equation.py
    
    Co-Authored-By: Andrew Head <head.andrewm@gmail.com>
    
    * small fix
    
    * added type
    
    * small updates
    
    * updates to the formatting
    
    Co-authored-by: Andrew Head <head.andrewm@gmail.com>

--- data-processing/tests/test_parse_equation.py
+++ data-processing/tests/test_parse_equation.py
@@ -85,3 +85,15 @@ def test_get_symbol_children():
     assert len(t_sub_i.children) == 2
     assert t in t_sub_i.children
     assert i in t_sub_i.children
+
+def test_parse_consecutive_mi():
+    with open(get_test_path(os.path.join("mathml", "relu.xml"))) as mathml_file:
+        mathml = mathml_file.read()
+        symbols = get_symbols(mathml)
+
+    assert len(symbols) == 1
+
+    relu = symbols[0]
+    assert len(relu.children) == 0
+    assert len(relu.characters) == 4
+    assert relu.mathml == "<mi>ReLU</mi>"
