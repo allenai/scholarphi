d579a9c47b0c7eb9765e1c35934f0a87eef675c2
Daniel Estevan McAndrew <daniel.e.mcandrew@gmail.com>
2020-03-17 13:45:25 -0700

    Now able to more easily run on larger batches of multiple papers

--- data-processing/formatting/train_model.py
+++ data-processing/formatting/train_model.py
@@ -86,7 +86,7 @@ if __name__ == "__main__":
     cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
     cfg.SOLVER.IMS_PER_BATCH = 1
     cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
-    cfg.SOLVER.MAX_ITER = 10    # 10 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
+    cfg.SOLVER.MAX_ITER = 300    # 300 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
     cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (Eqbox)
     
@@ -95,6 +95,11 @@ if __name__ == "__main__":
     trainer.resume_or_load(resume=False)
     trainer.train()
 
+    cfg.MODEL.WEIGHTS = os.path.join(cfg.OUTPUT_DIR, "model_final.pth")
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.7   # set the testing threshold for this model
+    cfg.DATASETS.TEST = ("Eqbox_val", )
+    predictor = DefaultPredictor(cfg)
+    
     evaluator = COCOEvaluator("Eqbox_val", cfg, False, output_dir="./output/")
     val_loader = build_detection_test_loader(cfg, "Eqbox_val")
     inference_on_dataset(trainer.model, val_loader, evaluator)
