c64756764c074a699fd46bad066856e299bece34
Daniel Estevan McAndrew <daniel.e.mcandrew@gmail.com>
2020-03-16 21:28:18 -0700

    Adding code to train and evaluate a detectron2 RCNN model on bounding box data.

---
+++ data-processing/formatting/train_model.py
@@ -0,0 +1,101 @@
+import os
+import sys
+
+import json
+
+import numpy as np
+import pandas as pd
+import random
+import cv2
+
+# You may need to restart your runtime prior to this, to let your installation take effect
+# Some basic setup:
+# Setup detectron2 logger
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+
+from detectron2.structures import BoxMode
+from detectron2.data import DatasetCatalog, MetadataCatalog
+
+from detectron2.engine import DefaultTrainer
+from detectron2.config import get_cfg
+
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from detectron2.data import build_detection_test_loader
+
+
+def get_box_dicts(img_dir):
+    json_file = os.path.join(img_dir, "bounding_box_data.json")
+    with open(json_file) as f:
+        imgs_anns = json.load(f)
+
+    dataset_dicts = []
+    for idx, v in enumerate(imgs_anns.values()):
+        record = {}
+        
+        filename = os.path.join(img_dir, v["filename"])
+        height, width = cv2.imread(filename).shape[:2]
+        
+        record["file_name"] = filename
+        record["image_id"] = idx
+        record["height"] = height
+        record["width"] = width
+      
+        annos = v["regions"]
+        objs = []
+        for _, anno in annos.items():
+            assert not anno["region_attributes"]
+            anno = anno["shape_attributes"]
+            px = anno["all_points_x"]
+            py = anno["all_points_y"]
+            poly = [(x + 0.5, y + 0.5) for x, y in zip(px, py)]
+            poly = [p for x in poly for p in x]
+
+            obj = {
+                "bbox": [np.min(px), np.min(py), np.max(px), np.max(py)],
+                "bbox_mode": BoxMode.XYXY_ABS,
+                "segmentation": [poly],
+                "category_id": 0,
+                "iscrowd": 0
+            }
+            objs.append(obj)
+        record["annotations"] = objs
+        dataset_dicts.append(record)
+    return dataset_dicts  
+                   
+        
+if __name__ == "__main__":
+    for d in ["train", "val"]:
+        DatasetCatalog.register("Eqbox_" + d, lambda d=d: get_box_dicts(sys.argv[1] + d))
+        MetadataCatalog.get("Eqbox_" + d).set(thing_classes=["Eqbox"])
+    Eqbox_metadata = MetadataCatalog.get("Eqbox_train")
+
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml"))
+    cfg.DATASETS.TRAIN = ("Eqbox_train",)
+    cfg.DATASETS.TEST = ()
+    cfg.DATALOADER.NUM_WORKERS = 1
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-InstanceSegmentation/mask_rcnn_R_50_FPN_3x.yaml")  # Let training initialize from model zoo
+    cfg.SOLVER.IMS_PER_BATCH = 1
+    cfg.SOLVER.BASE_LR = 0.00025  # pick a good LR
+    cfg.SOLVER.MAX_ITER = 10    # 10 iterations seems good enough for this toy dataset; you may need to train longer for a practical dataset
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 128   # faster, and good enough for this toy dataset (default: 512)
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1  # only has one class (Eqbox)
+    
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+    trainer = DefaultTrainer(cfg) 
+    trainer.resume_or_load(resume=False)
+    trainer.train()
+
+    evaluator = COCOEvaluator("Eqbox_val", cfg, False, output_dir="./output/")
+    val_loader = build_detection_test_loader(cfg, "Eqbox_val")
+    inference_on_dataset(trainer.model, val_loader, evaluator)
+    # another equivalent way is to use trainer.test
