b46c346bc6d21c41edf8b13bed06a742aed04bc9
jusheen <jusheen9@gmail.com>
2020-04-28 00:44:02 -0700

    Added a test for multi terms, cleaned code by creating a GlossaryTerm object, added in TODO's for future

--- data-processing/entities/terms/extractor.py
+++ data-processing/entities/terms/extractor.py
@@ -10,20 +10,24 @@ from common.parse_tex import DEFAULT_CONTEXT_SIZE, EntityExtractor, PlaintextExt
 from common.types import SerializableEntity
 
 
-
-# Edit location #1: Define a Term entity; the below code is probably okay.
 @dataclass(frozen=True)
 class Term(SerializableEntity):
 	name: str
 	defintion_text: str
 
-	#list if there is more than one string for the definition html
-	definition_html: Union[str,list]
+	# List if there is more than one string for the definition html; str otherwise.
+	definition_html: Union[str, list]
 
 
-# Edit location #2: Create a term extractor;
-# It should include your test code from last week; it just has an output
-# in a slightly different shape.
+class GlossaryTerm:
+	def __init__(self, gloss_term, gloss_def_text, gloss_def_html):
+		self.term = gloss_term
+		self.def_text = gloss_def_text
+		self.def_html = gloss_def_html
+
+
+
+
 class TermExtractor(EntityExtractor):
 	"""
 	Extract plaintext terms from TeX, with the locations of the start and end characters they correspond to in
@@ -42,16 +46,16 @@ class TermExtractor(EntityExtractor):
 		plaintext_segments = plaintext_extractor.parse(tex_path, tex)
 
 		# Extracting all the terms, definition_text and definition_html from glossary
-		terms_and_def_text_html = []
+		# TODO: DELETE THE COMMENT BELOW AFTER MAKING DECISION
+		# Not sure if concise_glossary is necessary; object properties of list type
+		# might come in handy later on.
+		concise_glossary = []
 
-		for obj in self.glossary:
-			new_triple = []
-
-			# Making terms lower case in one step
-			new_triple.append(obj["terms"].lower())
-			new_triple.append(obj["definition_text"])
-			new_triple.append(obj["definition_html"])
-			terms_and_def_text_html.append(new_triple)
+		for glossary_word in self.glossary:
+			new_glossary_word = GlossaryTerm(glossary_word["terms"].lower(),
+											 glossary_word["definition_text"],
+											 glossary_word["definition_html"])
+			concise_glossary.append(new_glossary_word)
 
 		matched_terms = []
 		matched_definition_texts = []
@@ -66,33 +70,45 @@ class TermExtractor(EntityExtractor):
 			# Making two lists from the text_segment_string; one is to capture words without dashes
 			# The other is to capture words such as "Gradient-Activated" (but this word happens to not be
 			# in the glossary
+			# TODO: Split on periods, commas, any special character to catch cases with
+			#  trailing punctuation such as "segmentation."
 			text_segment_string_split_on_dashes = re.split(
-				"[-\s+]", text_segment_string)
+				"[-\s]+", text_segment_string)
 			text_segment_string_not_split_on_dashes = re.split(
-				"[\s+]", text_segment_string)
+				"[\s]+", text_segment_string)
 
 			# Adding all terms that were both in the glossary and in the .tex file into a
 			# set so we don't get duplicates
+			# TODO: Make python class for matches_tuples; or utilize term object class
 			matches_tuples = []
 			matches = set()
 			for word in text_segment_string_split_on_dashes:
-				for object in terms_and_def_text_html:
+				for glossary_word in concise_glossary:
 
 					# Object[0] is the term
-					if object[0] == word:
-
+					if glossary_word.term == word:
 						# Adding term name, text_segment from which term was found, def text and def html
-						matches_tuples.append([object[0], text_segment_string, object[1], object[2]])
+						matches_tuples.append([glossary_word.term,
+											   text_segment_string,
+											   glossary_word.def_text,
+											   glossary_word.def_html])
 
-			for word2 in text_segment_string_not_split_on_dashes:
-				for object in terms_and_def_text_html:
-					if object[0] == word2:
-						matches_tuples.append([object[0], text_segment_string, object[1], object[2]])
-			for object in terms_and_def_text_html:
+			for word in text_segment_string_not_split_on_dashes:
+				for glossary_word in concise_glossary:
+					if glossary_word.term == word:
+						matches_tuples.append([glossary_word.term,
+											   text_segment_string,
+											   glossary_word.def_text,
+											   glossary_word.def_html])
+
+			for glossary_word in concise_glossary:
 				term_list = re.split("\s+", object[0])
 				if len(term_list) > 1:
-					if object[0] in text_segment_string:
-						matches_tuples.append([object[0], text_segment_string, object[1], object[2]])
+					if glossary_word.term in text_segment_string:
+						matches_tuples.append([glossary_word.term,
+											   text_segment_string,
+											   glossary_word.def_text,
+											   glossary_word.def_html])
 
 			for i in matches_tuples:
 				list_to_add = i[:-1]
@@ -131,7 +147,6 @@ class TermExtractor(EntityExtractor):
 
 				# 2) Keep the object with the largest end index.
 				if len(duplicate_indices) > 1:
-					print("OH NO")
 					ending_indices = []
 					for duplicate_index in duplicate_indices:
 						ending_indices.append(matched_terms_end[duplicate_index])
@@ -154,23 +169,21 @@ class TermExtractor(EntityExtractor):
 
 		delete_duplicate_start(matched_terms, matched_terms_start, matched_terms_end, matched_terms_context_str)
 
+		# TODO: Order terms objects by their start index.
+
 		index = 0
-		for term_object_number in range(len(matched_terms)):
+		for term_object_number in enumerate(matched_terms):
 			start = matched_terms_start[term_object_number]
 			end = matched_terms_end[term_object_number]
 			yield Term(
 				name=matched_terms[term_object_number],
 				defintion_text=matched_definition_texts[term_object_number],
 				definition_html=matched_definition_htmls[term_object_number],
-				# start position of the term in the TeX
 				start=matched_terms_start[term_object_number],
-				end=matched_terms_end[term_object_number],  # end position
-				id_=str(index),  # unique ID for each term (index)
-				# tex_path (what was passed into the function)
+				end=matched_terms_end[term_object_number],
+				id_=str(index),
 				tex_path=tex_path,
-				tex=tex,  # includes all of the spaces and the LaTeX junk
-				# context_tex=term_object["context_str"],  # tex + 20 characters before + 20 characters after
-				# Assuming DEFAULT_CONTEXT_SIZE is 20 characters
+				tex=tex,
 				context_tex=tex[start - \
 								DEFAULT_CONTEXT_SIZE: end + DEFAULT_CONTEXT_SIZE]
 			)
--- data-processing/entities/terms/test_terms.py
+++ data-processing/entities/terms/test_terms.py
@@ -10,9 +10,11 @@ import json
 # from common.parse_tex import DEFAULT_CONTEXT_SIZE, EntityExtractor, PlaintextExtractor
 # from common.types import SerializableEntity
 
+# TODO: Add asserts for definition text & definition html.
+
 glossary = json.load(open(os.path.join('resources', 'aggregate_glossary.json')))
 
-def test_extract_terms(terms):
+def test_extract_single_terms(terms):
     extractor = TermExtractor(input_glossary=glossary)
     terms = list(
         extractor.parse(
@@ -38,3 +40,64 @@ def test_extract_terms(terms):
     assert term3.start == 73
     assert term3.end == 77
     assert term3.context_tex == "/title{towards interpretable semantic segmentation via gradient-weighted class activation mapping}"
+
+def test_extract_multiple_terms(terms):
+    extractor = TermExtractor(input_glossary=glossary)
+    terms = list(
+        extractor.parse(
+            "main.tex",
+        "approaches based on deep learning, and convolutional neural networks (cnns) in particular,"
+        " have recently substantially improved the performance for various image understanding tasks,"
+        " such as image classification, object detection, and image segmentation.")
+    )
+
+    assert len(terms) == 5
+    term1 = terms[0]
+    assert term1.term == "deep learning"
+    assert term1.start == 21
+    assert term1.end == 33
+    assert term1.context_tex == "approaches based on deep learning, and convolutional neural networks (cnns) in particular, " \
+                                "have recently substantially improved the performance for various image understanding tasks, " \
+                                "such as image classification, object detection, and image segmentation."
+
+    term2 = terms[1]
+    assert term2.term == "convolutional neural network"
+    assert term2.start == 40
+    assert term2.end == 68
+    assert term2.context_tex == "approaches based on deep learning, and convolutional neural networks (cnns) in particular, " \
+                                "have recently substantially improved the performance for various image understanding tasks, " \
+                                "such as image classification, object detection, and image segmentation."
+
+    term3 = terms[2]
+    assert term3.term == "neural networks"
+    assert term3.start == 55
+    assert term3.end == 69
+    assert term3.context_tex == "approaches based on deep learning, and convolutional neural networks (cnns) in particular, " \
+                                "have recently substantially improved the performance for various image understanding tasks, " \
+                                "such as image classification, object detection, and image segmentation."
+
+    term4 = terms[3]
+    assert term4.term == "performance"
+    assert term4.start == 134
+    assert term4.end == 144
+    assert term4.context_tex == "approaches based on deep learning, and convolutional neural networks (cnns) in particular, " \
+                                "have recently substantially improved the performance for various image understanding tasks, " \
+                                "such as image classification, object detection, and image segmentation."
+
+    term5 = terms[4]
+    assert term5.term == "object"
+    assert term5.start == 215
+    assert term5.end == 220
+    assert term5.context_tex == "approaches based on deep learning, and convolutional neural networks (cnns) in particular, " \
+                                "have recently substantially improved the performance for various image understanding tasks, " \
+                                "such as image classification, object detection, and image segmentation."
+
+    term6 = terms[5]
+    assert term6.term == "segmentation"
+    assert term6.start == 243
+    assert term6.end == 254
+    assert term6.context_tex == "approaches based on deep learning, and convolutional neural networks (cnns) in particular, " \
+                                "have recently substantially improved the performance for various image understanding tasks, " \
+                                "such as image classification, object detection, and image segmentation."
+
+
