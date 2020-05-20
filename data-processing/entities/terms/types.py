0f5bfed49c406567d5633c295489487b16fd24a6
Raymond Fok <rayfok@umich.edu>
2020-05-20 13:19:59 -0700

    add term extraction to data-processing pipeline

---
+++ data-processing/entities/terms/types.py
@@ -0,0 +1,16 @@
+from dataclasses import dataclass
+from typing import List, Union
+
+from common.types import SerializableEntity
+
+
+@dataclass(frozen=True)
+class Term(SerializableEntity):
+    name: str
+    definition: str
+
+
+@dataclass(frozen=True)
+class GlossaryTerm:
+    name: str
+    definition: Union[str, List[str]]
