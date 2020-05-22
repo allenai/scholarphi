ab4839520e07a3e3c34ef2b3862349e06e1b640b
Daniel Estevan McAndrew <daniel.e.mcandrew@gmail.com>
2020-05-22 16:50:54 -0700

    changed it to run with path to the data directory instead of 22-hue-locations-for-equations. The needed subdirectories 06-paper-images, 17-detected-equations, and 22-hue-locations-for-equations are expected to be inside this data directory.

--- data-processing/formatting/prep_training_data.py
+++ data-processing/formatting/prep_training_data.py
@@ -150,15 +150,15 @@ def group_near_eqns(df, all_Tex=False, group=True):
 
     return df 
     
-def join_hue_locs_and_entites(arxivIds, include_inline=False):
+def join_hue_locs_and_entites(data_dir, arxivIds, include_inline=False):
     '''Joins the information about where the equations are located in 
        the paper and what the equations are into a single csv file.'''
     for arxivId in arxivIds:
         if arxivId=='.DS_Store':
             continue
 
-        hueCsv = os.path.join("data", "22-hue-locations-for-equations", arxivId, "hue_locations.csv")
-        entCsv = os.path.join("data", "17-detected-equations", arxivId, "entities.csv")
+        hueCsv = os.path.join(data_dir, "22-hue-locations-for-equations", arxivId, "hue_locations.csv")
+        entCsv = os.path.join(data_dir, "17-detected-equations", arxivId, "entities.csv")
         # Read csvs:
         dfHue = pd.read_csv(hueCsv)
         dfEnt = pd.read_csv(entCsv)
@@ -177,14 +177,14 @@ def join_hue_locs_and_entites(arxivIds, include_inline=False):
         # match equations and their locations by idx: (note that one of them is 0-indexed and the other is 1-indexed, hence the -1
         dfHue["tex"] = dfHue['entity_id'].apply(lambda x: Idx_to_Tex.get(x))
         # Set and create the output directroy:
-        outDir = os.path.join("data", "99-formatting-data", arxivId)
+        outDir = os.path.join(data_dir, "99-formatting-data", arxivId)
         if not os.path.exists(outDir):
             os.makedirs(outDir)
         # Filter out to get only display equations. Comment out this line and
         # write dfHue to csv instead of dfDisplay to get all equations.
         dfDisplay = filter_display_eqs(dfHue, include_inline=include_inline)
 
-        imgDirPrefix = os.path.join("data", "06-paper-images", arxivId)
+        imgDirPrefix = os.path.join(data_dir, "06-paper-images", arxivId)
         imgDirSuffix = os.listdir(imgDirPrefix)
         imgPath = os.path.join(imgDirPrefix, imgDirSuffix[0],'page-1.png')
         img = cv2.imread(imgPath)
@@ -197,17 +197,17 @@ def join_hue_locs_and_entites(arxivIds, include_inline=False):
         dfGrouped.to_csv(os.path.join(outDir, "eqs_and_locs.csv"), index=False)
 
 
-def draw_boxes(arxivIds):
+def draw_boxes(data_dir, arxivIds):
     '''Reads in the paper image files and bounbding box csv file and draws the boxes onto the corresponding paper page.'''
     for arxivId in arxivIds:
         if arxivId=='.DS_Store':
             continue
 
-        outDir = os.path.join("data", "99-formatting-data", arxivId)
+        outDir = os.path.join(data_dir, "99-formatting-data", arxivId)
         eqLocsCsv = os.path.join(outDir, "eqs_and_locs.csv")
     
         dfEqs = pd.read_csv(eqLocsCsv)
-        imgDirPrefix = os.path.join("data", "06-paper-images", arxivId)
+        imgDirPrefix = os.path.join(data_dir, "06-paper-images", arxivId)
         imgDirSuffix = os.listdir(imgDirPrefix)
         imgDir = os.path.join(imgDirPrefix, imgDirSuffix[0])
         paperImgFiles = os.listdir(imgDir)
@@ -237,7 +237,7 @@ def draw_boxes(arxivIds):
             cv2.imwrite(os.path.join(outDir, paperImgFile), paperImg)
             
 
-def create_training_json(arxivIds, train=True, micro_eval=False):
+def create_training_json(data_dir, arxivIds, train=True, micro_eval=False):
     '''For a given list of arxiv Ids, this function creates a JSON file 
        that represents the labelled bounding box data for the pages of the 
        paper in the format expected by detectron2's object detection. 
@@ -253,12 +253,12 @@ def create_training_json(arxivIds, train=True, micro_eval=False):
 
 
     if train:
-        outDir = os.path.join("data", "CNNTest", "train")
+        outDir = os.path.join(data_dir, "CNNTest", "train")
     else:
         if not micro_eval:
-            outDir = os.path.join("data", "CNNTest", "val")
+            outDir = os.path.join(data_dir, "CNNTest", "val")
         else:
-            outDirs = [os.path.join("data", "CNNTest", "val_" + arxivId) for arxivId in arxivIds]
+            outDirs = [os.path.join(data_dir, "CNNTest", "val_" + arxivId) for arxivId in arxivIds]
 
     # Clear directory or directories (in the case of vla and micro_evaluation)
     if not micro_eval:
@@ -277,15 +277,14 @@ def create_training_json(arxivIds, train=True, micro_eval=False):
                 continue
 
             # Get the joined csv file for equation locatyions:
-            inDir = os.path.join("data", "99-formatting-data", arxivId)
+            inDir = os.path.join(data_dir, "99-formatting-data", arxivId)
             eqLocsCsv = os.path.join(inDir, "eqs_and_locs.csv")
             dfEqs = pd.read_csv(eqLocsCsv)
             # Get the paper image locations
-            imgDirPrefix = os.path.join("data", "06-paper-images", arxivId)
+            imgDirPrefix = os.path.join(data_dir, "06-paper-images", arxivId)
             imgDirSuffix = os.listdir(imgDirPrefix)
             imgDir = os.path.join(imgDirPrefix, imgDirSuffix[0])
         
-            #imgDir = os.path.join("data", "06-paper-images", arxivId, "main.pdf")
             paperImgFiles = os.listdir(imgDir)
             # Loop trhoguh paper's pages:
             for i in range(len(paperImgFiles)):
@@ -362,15 +361,14 @@ def create_training_json(arxivIds, train=True, micro_eval=False):
             # Dictionary that will become the json
             outDicts.append({})
             # Get the joined csv file for equation locatyions:
-            inDir = os.path.join("data", "99-formatting-data", arxivId)
+            inDir = os.path.join(data_dir, "99-formatting-data", arxivId)
             eqLocsCsv = os.path.join(inDir, "eqs_and_locs.csv")
             dfEqs = pd.read_csv(eqLocsCsv)
             # Get the paper image locations
-            imgDirPrefix = os.path.join("data", "06-paper-images", arxivId)
+            imgDirPrefix = os.path.join(data_dir, "06-paper-images", arxivId)
             imgDirSuffix = os.listdir(imgDirPrefix)
             imgDir = os.path.join(imgDirPrefix, imgDirSuffix[0])
         
-            #imgDir = os.path.join("data", "06-paper-images", arxivId, "main.pdf")
             paperImgFiles = os.listdir(imgDir)
             # Loop trhoguh paper's pages:
             for i in range(len(paperImgFiles)):
@@ -438,7 +436,7 @@ def finerFiltering(arxivIds):
                 continue
 
             # Get the joined csv file for equation locatyions:
-            inDir = os.path.join("data", "99-formatting-data", arxivId)
+            inDir = os.path.join(data_dir, "99-formatting-data", arxivId)
             eqLocsCsv = os.path.join(inDir, "eqs_and_locs.csv")
             dfEqs = pd.read_csv(eqLocsCsv)
             dfEqs['paper'] = arxivId
@@ -475,18 +473,19 @@ def finerFiltering(arxivIds):
 
 
 if __name__ == "__main__":
-    # Run with arxiv Ids as commandline args:
-    locs_dir = sys.argv[1]
-    arxivIds = os.listdir(locs_dir)
+    # Run with path to the data directory. Inside this directory should be
+    # 22-hue-locations-for-equations, 17-detected-equations, and 06-paper-images:
+    data_dir = sys.argv[1]
+    arxivIds = os.listdir(os.path.join(data_dir, '22-hue-locations-for-equations'))
     
     start = datetime.datetime.now()
     # Prep the data by joining the equation data with corresponding bounding box data:
     print('Step 1/6 : Join Eqn and Bounding Box data, group them')
-    join_hue_locs_and_entites(arxivIds, include_inline = True)
+    join_hue_locs_and_entites(data_dir, arxivIds, include_inline = True)
     
     # # Todo: eventually only draw boxes if some kind of debug flag is set:
     print('Step 2/6 : Draw Bounding Boxes')
-    #draw_boxes(arxivIds)
+    #draw_boxes(data_dir, arxivIds)
 
     print('Step 3/6 : More filtering based on aggregate distributions, and exceptions')
     final_arxivIds = finerFiltering(arxivIds)
@@ -496,9 +495,9 @@ if __name__ == "__main__":
     train_arxivs, val_arxivs = train_val_split(final_arxivIds)
     # Prepare the json files for the RCNN model:
     print('Step 5/6 : Create Train set')
-    create_training_json(train_arxivs, train=True)
+    create_training_json(data_dir, train_arxivs, train=True)
     print('Step 6/6 : Create Valid set')
-    create_training_json(val_arxivs, train=False)#, micro_eval=True)
+    create_training_json(data_dir, val_arxivs, train=False)#, micro_eval=True)
     end = datetime.datetime.now()
 
     print(f"Total processing time : {(end-start).total_seconds()/60} mins")
