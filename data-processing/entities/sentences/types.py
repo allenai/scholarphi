c1cfa69e6dcb1dd2fedcc9d8b8c32d08d463d7bc
Dongyeop Kang <dykang85@gmail.com>
2020-06-26 13:23:55 -0400

    initial version of improvement for SentenceExtractor  - additional fields: cleaned_text, is_sentence, current_{section,figure,table}, label, ref, cite, symbol, others  - detecting richer tex context from context_tex  - regex for detecting various tex entities (e.g., section, label, abstract, table, figure, reference, citation, symbol, etc)  - cleaned_text has all reference entities with placeholders like CITATION, SYMBOL, TABLE, FIGURE, SECTION, and EQUATION.

--- data-processing/entities/sentences/types.py
+++ data-processing/entities/sentences/types.py
@@ -6,3 +6,14 @@ from common.types import SerializableEntity
 @dataclass(frozen=True)
 class Sentence(SerializableEntity):
     text: str
+    cleaned_text: str
+    is_sentence: bool
+    current_section: str
+    current_figure: str
+    current_table: str
+    label: str
+    ref: str
+    cite: str
+    symbol: str
+    others: str
+
