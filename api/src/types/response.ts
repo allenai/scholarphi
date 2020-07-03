86fea01acc93faeaae58eec17dd69e98014783f7
Andrew Head <head.andrewm@gmail.com>
2020-07-02 23:26:09 -0700

    API: Started work on providing a new API for entities

---
+++ api/src/types/response.ts
@@ -0,0 +1,173 @@
+/**
+ * The design of the data types for responses roughly follow the conventions of JSON:API:
+ * See the specification at https://jsonapi.org/format/.
+ *
+ * In addition, field names are written using underscores, not camelCase.
+ *
+ * These data structures roughly resemble the schemas in the database. A definition of the schemas along
+ * with documentation should reside in 'data-processing/common/models.py' or thereabouts.
+ *
+ * This file was intended to be copied and pasted into other projects that need to know the types
+ * returned from the API. Because of this, all types are exported, so that when this file is copied
+ * into other projects, all of the types are available to the client code.
+ */
+
+/**
+ * Use type guards (e.g., 'isSymbol') to distinguish between types of entities.
+ */
+export type Entity = GenericEntity | Symbol | Citation | Sentence;
+
+/**
+ * All entity specifications should extend BaseEntity. To make specific relationship properties
+ * known to TypeScript, define a relationships type (e.g., see 'SymbolRelationships'), and
+ * pass it as the first generic parameter. Otherwise, set the generic to '{}'.
+ */
+export interface BaseEntity {
+  id: string;
+  type: string;
+  attributes: BaseEntityAttributes;
+  relationships: Relationships;
+}
+
+/**
+ * All entities must define at least these attributes.
+ */
+export interface BaseEntityAttributes {
+  version: number;
+  source: string;
+  bounding_boxes: BoundingBox[];
+}
+
+/**
+ * List of base entity attribute keys. Update this as 'BaseEntityAttributes' updates. This list
+ * lets a program check statically whether an attribute on an entity is a custom attribute.
+ */
+export const BASE_ENTITY_ATTRIBUTE_KEYS = [
+  "id",
+  "type",
+  "version",
+  "source",
+  "bounding_boxes",
+];
+
+/**
+ * While it is not described with types here, Relationships must be key-value pairs, where the values
+ * are either 'Relationship' or a list of 'Relationship's.
+ */
+export interface Relationships {}
+
+export interface Relationship {
+  type: string;
+  id: string | null;
+}
+
+export function isRelationship(o: any): o is Relationship {
+  return typeof o.type === "string" && typeof o.id === "string";
+}
+
+/**
+ * An entity where no information is known about its type, and hence attributes and
+ * relationships specific to that entity type are not available. This type is defined to provide
+ * *some* small amount of type checking for unknown and new types of entities.
+ */
+export interface GenericEntity extends BaseEntity {
+  attributes: BaseEntityAttributes & GenericAttributes;
+  relationships: GenericRelationships;
+}
+
+/**
+ * In general, attributes can have values of type 'string | number | string[] | null | undefined'. Because
+ * we're doing a type intersection with BaseEntityAttributes above to define the attributes for
+ * a BasicEntity (where some properties are of other types like BoundingBox[]), 'any' is used as
+ * the type of value here, even though the types of GenericAttributes values should be restricted.
+ */
+export interface GenericAttributes {
+  [key: string]: any;
+}
+
+export type GenericRelationships = {
+  [key: string]: Relationship | Relationship[];
+};
+
+/**
+ * 'Symbol' is an example of how to define a new entity type with custom attributes
+ * and relationships. Note that a full custom entity definition includes:
+ * * an 'attributes' type
+ * * a 'relationships' type
+ * * a type-guard (i.e., an 'is<Entity>' function)
+ */
+export interface Symbol extends BaseEntity {
+  type: "symbol";
+  attributes: SymbolAttributes;
+  relationships: SymbolRelationships;
+}
+
+export interface SymbolAttributes extends BaseEntityAttributes {
+  mathml: string;
+  mathml_near_matches: string[];
+}
+
+export interface SymbolRelationships {
+  sentence: Relationship;
+  children: Relationship[];
+}
+
+/**
+ * Type guards for entities should only have to check the 'type' attribute.
+ */
+export function isSymbol(entity: Entity): entity is Symbol {
+  return entity.type === "symbol";
+}
+
+export interface Citation extends BaseEntity {
+  type: "citation";
+  attributes: CitationAttributes;
+  relationships: {};
+}
+
+export interface CitationAttributes extends BaseEntityAttributes {
+  paper_id: string;
+}
+
+export function isCitation(entity: Entity): entity is Citation {
+  return entity.type === "citation";
+}
+
+export interface Sentence extends BaseEntity {
+  type: "sentence";
+  attributes: SentenceAttributes;
+  relationships: {};
+}
+
+export interface SentenceAttributes extends BaseEntityAttributes {
+  text: string;
+  tex_start: number;
+  tex_end: number;
+}
+
+export function isSentence(entity: Entity): entity is Sentence {
+  return entity.type === "sentence";
+}
+
+/**
+ * Bounding box containing where an entity appears. Coordinates (left, top, width, and height) are all
+ * in ratios from [0..1] relative to the actual width and height of the page.
+ */
+export interface BoundingBox {
+  source: string;
+  /**
+   * Page indexes start at 0.
+   */
+  page: number;
+  left: number;
+  top: number;
+  width: number;
+  height: number;
+}
+
+export interface PaperWithEntityCounts {
+  s2_id: string;
+  arxiv_id?: string;
+  citations: string;
+  symbols: string;
+}
