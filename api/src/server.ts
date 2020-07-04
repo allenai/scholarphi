f0350990f7bd6c812aed8547f07b1b286fa94c88
Andrew Head <head.andrewm@gmail.com>
2020-07-04 12:53:22 -0700

    API: Close database connection on server.destroy()

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,34 +1,101 @@
-import { Server } from "@hapi/hapi";
+import { Server, ServerInjectOptions } from "@hapi/hapi";
 import * as nconf from "nconf";
+import * as api from "./api";
+import { Connection, extractConnectionParams } from "./db-connection";
 
-export const init = async (config: nconf.Provider) => {
-  const server = new Server({
-    port: 3000,
-    host: "0.0.0.0",
-    debug: {
-      request: ["error"],
-    },
-  });
+/**
+ * Wrapper around hapi server that takes care of stateful server start and stop operations.
+ */
+class ApiServer {
+  constructor(config: nconf.Provider) {
+    this._config = config;
+  }
 
-  await server.register({
-    plugin: require("./api"),
-    options: { config },
-    routes: {
-      prefix: "/api/v0/",
-    },
-  });
+  async init() {
+    this._server = new Server({
+      port: 3000,
+      host: "0.0.0.0",
+      debug: {
+        request: ["error"],
+      },
+    });
 
-  server.route({ method: "GET", path: "/health", handler: () => "👍" });
-  return server;
-};
+    /**
+     * The database connection is initialized in the server object, instead of
+     * in the plugin code, because a reference to the connection needs to be saved
+     * so that the connection can be closed when the server is shut down.
+     */
+    const connectionParams = extractConnectionParams(this._config);
+    this._dbConnection = new Connection(connectionParams);
 
-export const start = async (server: Server) => {
-  await server.start();
-  console.log("Server running on %s", server.info.uri);
+    /**
+     * Register API endpoints.
+     */
+    await this._server.register({
+      plugin: api.plugin,
+      options: { connection: this._dbConnection },
+      routes: {
+        prefix: "/api/v0/",
+      },
+    });
 
-  server.events.on("log", (event, tags) => {
-    if (tags.error) {
-      console.log(`Server error: ${event.error ? event.error : "unknown"}`);
+    this._server.route({ method: "GET", path: "/health", handler: () => "👍" });
+  }
+
+  async start() {
+    if (this._server === null) {
+      console.error(
+        "start() called on server before it was initialized. Call init() first."
+      );
+      return;
     }
-  });
-};
+
+    await this._server.start();
+    console.log("Server running on %s", this._server.info.uri);
+
+    this._server.events.on("log", (event, tags) => {
+      if (tags.error) {
+        console.log(`Server error: ${event.error ? event.error : "unknown"}`);
+      }
+    });
+  }
+
+  async stop() {
+    if (this._server === null) {
+      return;
+    }
+    await this._server.stop();
+  }
+
+  /**
+   * Destroy the resources associated with this server (e.g., database connections).
+   * Should be called after 'stop' if the server is started.
+   */
+  async destroy() {
+    if (this._dbConnection === null) {
+      console.error(
+        "destroy() called on server before it was initialized. Call init() first."
+      );
+      return;
+    }
+    await this._dbConnection.close();
+  }
+
+  /**
+   * Exposed only for test purposes. If called in production, should be wrapped with error handler.
+   */
+  inject(options: string | ServerInjectOptions) {
+    if (this._server === null) {
+      throw Error(
+        "inject() called on server before it was initialized. Call init() first."
+      );
+    }
+    return this._server.inject(options);
+  }
+
+  private _config: nconf.Provider;
+  private _server: Server | null = null;
+  private _dbConnection: Connection | null = null;
+}
+
+export default ApiServer;
