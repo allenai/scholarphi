a88738e114f21b91fab8d5ae43076cd36051ee8f
rishamsidhu <35544411+rishamsidhu@users.noreply.github.com>
2020-07-06 13:44:32 -0400

    Update extractor.py

--- data-processing/entities/abbreviations/extractor.py
+++ data-processing/entities/abbreviations/extractor.py
@@ -86,44 +86,44 @@ class AbbreviationExtractor(EntityExtractor):
         if last_segment is not None:
             plaintext_to_tex_offset_map[len(plaintext)] = last_segment.tex_end
 
-        #this is the most basic model and had no real performance difference on our inputs
-        #other options include NER models and models with pretrained word vectors
+        # This is the most basic model and had no real performance difference on our inputs,
+        # other options include NER models and models with pretrained word vectors.
         nlp = spacy.load("en_core_sci_sm")
         abbreviation_pipe = AbbreviationDetector(nlp)
         nlp.add_pipe(abbreviation_pipe)
 
-        #these dictionaries hold abbreviated forms, their expansions, and the location of the expansions
-        #all of them use the abbreviated form as keys
+        # These dictionaries hold abbreviated forms, their expansions, and the location of the expansions.
+        # All of them use the abbreviated form as keys.
         abb_short_forms = {}
         abb_expansions = {}
         expanded_locations = {}
         doc = nlp(plaintext)
 
-        #this extracts the abbreviations from the scispacy model
+        # This extracts the abbreviations from the scispacy model.
         for abrv in doc._.abbreviations:
             count = 0
             for s in NON_ACRONYM_CHARACTERS:
                  count += str(abrv).count(s)
-            #count makes sure that we don't accidentally include symbols or variables
+            # count makes sure that we don't accidentally include symbols or variables.
             if count == 0:
                 abb_short_forms[str(abrv)] = [[plaintext_to_tex_offset_map[m.start()], plaintext_to_tex_offset_map[m.start() + len(str(abrv))]] for m in re.finditer(str(abrv), plaintext)]
                 abb_expansions[str(abrv)] = str(abrv._.long_form)
                 x = plaintext.find(str(abrv._.long_form))
                 expanded_locations[str(abrv)] = [plaintext_to_tex_offset_map[x], plaintext_to_tex_offset_map[x + len(str(abrv._.long_form))]]
 
-        #if you want to use another abbreviation detection method in addition to scispacy
-        #you may implement it here and add its results to the three dictionaries
+        # If you want to use another abbreviation detection method in addition to scispacy
+        # you may implement it here and add its results to the three dictionaries.
 
         count = 0
-        fcount = 1
-        #yields abbreviated forms and their expansions
+        full_count = 1
+        # Yields abbreviated forms and their expansions.
         for abb in abb_short_forms:
             exp_start, exp_end = expanded_locations[abb]
             expanded = abb_expansions[abb]
             tex_sub = tex[exp_start:exp_end]
             context_tex = tex[exp_start - DEFAULT_CONTEXT_SIZE : exp_end + DEFAULT_CONTEXT_SIZE]
 
-            #yields the expanded form as an Abbreviation type
+            # Yields the expanded form as an Abbreviation type
             yield Abbreviation(
                 text= abb,
                 start=exp_start,
@@ -133,14 +133,14 @@ class AbbreviationExtractor(EntityExtractor):
                 tex_path=tex_path,
                 tex=tex_sub,
                 context_tex=context_tex,
-                str_id = "f" + str(fcount) + "-0"
+                str_id = "f" + str(full_count) + "-0"
             )
             count += 1
-            scount = 0
+            short_count = 0
 
-            #yields the abbreviated forms as Abbreviation types
+            # Yields the abbreviated forms as Abbreviation types.
             for location in abb_short_forms[abb]:
-                scount += 1
+                short_count += 1
                 start, end = location
                 tex_sub = tex[start:end]
                 context_tex = tex[start - DEFAULT_CONTEXT_SIZE : end + DEFAULT_CONTEXT_SIZE]
@@ -153,8 +153,8 @@ class AbbreviationExtractor(EntityExtractor):
                     tex_path=tex_path,
                     tex=tex_sub,
                     context_tex=context_tex,
-                    str_id = "s" + str(fcount) + "-" + str(scount)
+                    str_id = "s" + str(full_count) + "-" + str(short_count)
                 )
                 count += 1
 
-            fcount += 1
+            full_count += 1
