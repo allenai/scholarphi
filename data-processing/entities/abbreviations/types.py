1d9246285ae36422282044ac79a2272808bbaee4
rishamsidhu <35544411+rishamsidhu@users.noreply.github.com>
2020-07-06 13:34:00 -0400

    Update types.py

--- data-processing/entities/abbreviations/types.py
+++ data-processing/entities/abbreviations/types.py
@@ -7,7 +7,7 @@ from common.types import SerializableEntity
 class Abbreviation(SerializableEntity):
     text: str
     expansion : str
-    #the str_id tells us whether it is a full form or short form (f or s),
-    #which abbreviation it is (e.g. NLP might be 1 and GPU might be 2),
-    #and which instance of the abbreviation it is (i.e. 1 for the first instance, 2 for the second, and so on)
+    # The str_id tells us whether it is a full form or short form (f or s),
+    # which abbreviation it is (e.g. NLP might be 1 and GPU might be 2),
+    # and which instance of the abbreviation it is (i.e. 1 for the first instance, 2 for the second, and so on).
     str_id : str
