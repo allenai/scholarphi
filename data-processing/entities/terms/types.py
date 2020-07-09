f646377b4fd409ee1f68c76b857f9a8ae0120935
Andrew Head <head.andrewm@gmail.com>
2020-07-09 14:44:14 -0700

    Pipeline: Update term uploading to use the new upload function

--- data-processing/entities/terms/types.py
+++ data-processing/entities/terms/types.py
@@ -1,5 +1,4 @@
 from dataclasses import dataclass
-from typing import List, Union
 
 from common.types import SerializableEntity
 
--- data-processing/entities/terms/upload.py
+++ data-processing/entities/terms/upload.py
@@ -1,111 +1,31 @@
-import os
-from dataclasses import dataclass
-from typing import Dict, List, cast
+from typing import Optional, cast
 
-from peewee import DoesNotExist, ForeignKeyField, TextField
-
-from common import directories, file_utils
-from common.models import (
-    BoundingBox,
-    Entity,
-    EntityBoundingBox,
-    OutputModel,
-    Paper,
-    output_database,
-)
-from common.types import HueLocationInfo, PaperProcessingResult
+from common.types import BoundingBox, EntityInformation, PaperProcessingResult
+from common.upload_entities import upload_entities
 
 from .types import Term as TermEntity
 
 
-class Term(OutputModel):
-    paper = ForeignKeyField(Paper, on_delete="CASCADE")
-    name = TextField()
-    definition = TextField()
+def upload_sentences(
+    processing_summary: PaperProcessingResult, data_version: Optional[int]
+) -> None:
 
-
-@dataclass(frozen=True)
-class TermKey:
-    tex_path: str
-    entity_id: str
-
-
-@dataclass(frozen=True)
-class TermIdAndModelId(TermKey):
-    " Link between a term in the pipeline's output and its ID in the database. "
-    model_id: str
-
-
-def upload_terms(processing_summary: PaperProcessingResult) -> None:
-    arxiv_id = processing_summary.arxiv_id
-    s2_id = processing_summary.s2_id
-
-    # Create entry for the paper if it does not yet exist
-    try:
-        paper = Paper.get(Paper.s2_id == s2_id)
-    except DoesNotExist:
-        paper = Paper.create(s2_id=s2_id, arxiv_id=arxiv_id)
-
-    locations_by_term_id: Dict[TermKey, List[HueLocationInfo]] = {}
-    terms: Dict[TermKey, TermEntity] = {}
-    term_models: Dict[TermKey, Term] = {}
-
+    entity_infos = []
     for entity_and_location in processing_summary.localized_entities:
         term = cast(TermEntity, entity_and_location.entity)
-        term_model = Term(paper=paper, name=term.name, definition=term.definition)
+        boxes = [cast(BoundingBox, l) for l in entity_and_location.locations]
 
-        term_key = TermKey(term.tex_path, term.id_)
-        locations_by_term_id[term_key] = entity_and_location.locations
-        term_models[term_key] = term_model
-        terms[term_key] = term
-
-    with output_database.atomic():
-        Term.bulk_create(term_models.values(), 100)
-
-    model_ids_dir = directories.arxiv_subdir("terms-model-ids", arxiv_id)
-    if os.path.exists(model_ids_dir):
-        file_utils.clean_directory(model_ids_dir)
-    else:
-        os.makedirs(model_ids_dir)
-    output_ids_path = os.path.join(model_ids_dir, "model_ids.csv")
-    for id_, term_entity in terms.items():
-        file_utils.append_to_csv(
-            output_ids_path,
-            TermIdAndModelId(
-                tex_path=term_entity.tex_path,
-                entity_id=term_entity.id_,
-                model_id=term_models[id_].id,
-            ),
+        entity_info = EntityInformation(
+            id_=f"{term.tex_path}-{term.id_}",
+            type_="sentence",
+            bounding_boxes=boxes,
+            data={"name": term.name, "definition": term.definition,},
         )
+        entity_infos.append(entity_info)
 
-    entities = []
-    entity_bounding_boxes = []
-    bounding_boxes = []
-
-    for term_id, term_model in term_models.items():
-
-        entity = Entity.create(
-            type="term", source="tex-pipeline", entity_id=term_model.id
-        )
-        entities.append(entity)
-
-        for location in locations_by_term_id[term_id]:
-            bounding_box = BoundingBox.create(
-                page=location.page,
-                left=location.left,
-                top=location.top,
-                width=location.width,
-                height=location.height,
-            )
-            bounding_boxes.append(bounding_box)
-
-            entity_bounding_boxes.append(
-                EntityBoundingBox(bounding_box=bounding_box, entity=entity)
-            )
-
-    with output_database.atomic():
-        BoundingBox.bulk_create(bounding_boxes, 100)
-    with output_database.atomic():
-        Entity.bulk_create(entities, 300)
-    with output_database.atomic():
-        EntityBoundingBox.bulk_create(entity_bounding_boxes, 300)
+    upload_entities(
+        processing_summary.s2_id,
+        processing_summary.arxiv_id,
+        entity_infos,
+        data_version,
+    )
