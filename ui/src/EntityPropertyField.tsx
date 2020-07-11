c749ffffb092967f051ed8e36ea1a232427b55b4
Andrew Head <head.andrewm@gmail.com>
2020-07-11 11:54:58 -0700

    UI: Add support for editing list properties

--- ui/src/EntityPropertyField.tsx
+++ ui/src/EntityPropertyField.tsx
@@ -1,41 +1,141 @@
+import IconButton from "@material-ui/core/IconButton";
 import TextField from "@material-ui/core/TextField";
+import Add from "@material-ui/icons/Add";
+import DeleteForever from "@material-ui/icons/DeleteForever";
 import React from "react";
 import { Property } from "./EntityPropertyEditor";
-import ;
 
 interface Props {
   property: Property;
   value: any;
   error: string | undefined;
-  handlePropertyChanged: (
-    property: Property,
-    value: any,
-    index?: number
-  ) => void;
+  handlePropertyChanged: (property: Property, value: any) => void;
 }
 
 class EntityPropertyField extends React.PureComponent<Props> {
+  constructor(props: Props) {
+    super(props);
+    this.onFieldChanged = this.onFieldChanged.bind(this);
+    this.onClickAddItem = this.onClickAddItem.bind(this);
+    this.onClickDeleteItem = this.onClickDeleteItem.bind(this);
+  }
+
+  onFieldChanged(
+    event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) {
+    const currentValue = this.props.value;
+    const property = this.props.property;
+    const newValue = event.currentTarget.value;
+
+    /*
+     * If the property is a list, change the one item and keep the rest the same.
+     * Otherwise, the full value has changed.
+     */
+    if (property.is_list) {
+      const changeIndex = parseInt(
+        event.currentTarget.id.replace(`property-${property.key}-field-`, "")
+      );
+      const newValues = [...currentValue];
+      newValues[changeIndex] = newValue;
+      this.props.handlePropertyChanged(property, newValues);
+    } else {
+      this.props.handlePropertyChanged(property, newValue);
+    }
+  }
+
+  onClickAddItem(event: React.MouseEvent<HTMLButtonElement>) {
+    const { property, value } = this.props;
+    const addIndex = parseInt(
+      event.currentTarget.id
+        .replace(`property-${property.key}-field-`, "")
+        .replace("-add-button", "")
+    );
+    const currentValues = value as Array<string>;
+    const newValues = [...currentValues];
+    newValues.splice(addIndex + 1, 0, "");
+    this.props.handlePropertyChanged(property, newValues);
+  }
+
+  onClickDeleteItem(event: React.MouseEvent<HTMLButtonElement>) {
+    const { property, value } = this.props;
+    const deleteIndex = parseInt(
+      event.currentTarget.id
+        .replace(`property-${property.key}-field-`, "")
+        .replace("-delete-button", "")
+    );
+    const currentValues = value as Array<string>;
+    const newValues = [...currentValues];
+    newValues.splice(deleteIndex, 1);
+    this.props.handlePropertyChanged(property, newValues);
+  }
+
   render() {
+    const { property, value, error } = this.props;
+    const { label, is_list, key } = property;
+
+    const multiline =
+      property.type === "multiline-text" || property.type === "latex";
+
     return (
-      <>
-        {!this.props.property.is_list ? (
+      <div className="entity-property-field-container">
+        {!is_list ? (
           <TextField
+            className="entity-property-field"
+            InputProps={{ id: `property-${key}-field` }}
+            label={label}
+            error={error !== undefined}
+            helperText={error}
+            value={value}
             fullWidth={true}
-            label={this.props.property.label}
-            error={this.props.error !== undefined}
+            multiline={multiline}
+            rows={multiline ? 2 : 1}
+            rowsMax={multiline ? 3 : 1}
+            onChange={this.onFieldChanged}
           />
         ) : null}
-        {this.props.property.is_list ? (
+        {is_list ? (
           <>
-            {(this.props.value as Array).map(v => ({
-
-            })}
+            {(value as Array<any>).map((v, i) => (
+              <div className="entity-property-field-row" key={`field-row-${i}`}>
+                <TextField
+                  className="entity-property-field"
+                  InputProps={{ id: `property-${key}-field-${i}` }}
+                  /*
+                   * Show label above the first item, error message below the last iterm.
+                   */
+                  label={i === 0 ? label : undefined}
+                  error={error !== undefined}
+                  helperText={i === value.length - 1 ? error : undefined}
+                  value={v}
+                  multiline={multiline}
+                  rows={multiline ? 2 : 1}
+                  rowsMax={multiline ? 3 : 1}
+                  onChange={this.onFieldChanged}
+                />
+                <IconButton
+                  id={`property-${key}-field-${i}-add-button`}
+                  className="action-button"
+                  size="small"
+                  onClick={this.onClickAddItem}
+                >
+                  <Add />
+                </IconButton>
+                <IconButton
+                  id={`property-${key}-field-${i}-add-button`}
+                  className="action-button"
+                  size="small"
+                  onClick={this.onClickDeleteItem}
+                >
+                  <DeleteForever />
+                </IconButton>
+              </div>
+            ))}
           </>
-        ): null}
+        ) : null}
         {this.props.error !== undefined ? (
           <p className="entity-property-error">{this.props.error}</p>
         ) : null}
-      </>
+      </div>
     );
   }
 }
