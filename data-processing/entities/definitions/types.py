5903bb098138b56d4a7b5a7d0eee8e84658894de
Dongyeop Kang <dykang85@gmail.com>
2020-07-13 13:50:43 -0400

    add fields for Definition class

--- data-processing/entities/definitions/types.py
+++ data-processing/entities/definitions/types.py
@@ -6,3 +6,13 @@ from common.types import SerializableEntity
 @dataclass(frozen=True)
 class Definition(SerializableEntity):
     text: str
+    definitions: List[str]
+    " List of definitions, each coming from a different source. "
+    sources: List[str]
+    """
+    List of sources, one per definition. To find the source for a definition, look for the source
+    with the same index as the definition.
+    """
+    val: bool
+
+
