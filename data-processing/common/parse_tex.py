allenai/scholarphi

commit e7a6f523f834fa60ee90d67901208d0becbd37a8

Pipeline: fix missing sentences from sentence extractor

data-processing/common/parse_tex.py
@@ -388,8 +388,12 @@ def extract_plaintext(tex_path: str, tex: str) -> JournaledString:
 
     # Finally, remove adjacent periods (which interfere with the pysbd sentence
     # segmenter), which may only be adjacent because the TeX grouping has been removed.
-    for m in reversed(list(re.finditer(r"[\s\.]*\.", str(plaintext)))):
-        plaintext = plaintext.edit(m.start(), m.end(), ".")
+    # Do a lookahead for the last period (don't include it in the match) in order
+    # to change as little of the original TeX as possible, to make it easier to map
+    # back from the original period position (which will often occur at the end of
+    # an extracted sentence) to its precise position in the original TeX.
+    for m in reversed(list(re.finditer(r"[\s\.]+(?:\.)", str(plaintext)))):
+        plaintext = plaintext.edit(m.start(), m.end(), "")
 
     return plaintext
 

data-processing/entities/definitions/commands/detect_definitions.py
@@ -275,6 +275,12 @@ class DetectDefinitions(
             for si, sentence in enumerate(sentences_ordered):
                 progress.update(1)
 
+                # Only attempt to process sentences that have been marked as likely to be proper
+                # plaintext. Note that this means some sentences may be skipped that didn't pass
+                # heuristics in the sentence extractor.
+                if not sentence.validity_guess:
+                    continue
+
                 # Extract features from raw text.
                 featurized_text = model.featurize(sentence.legacy_definition_input)
                 features.append(featurized_text)

data-processing/entities/definitions/commands/embellish_sentences.py
@@ -154,7 +154,7 @@ class EmbellishSentences(ArxivBatchCommand[Task, EmbellishedSentence]):
                     span.start, span.end, "[[FORMULA]]"
                 )
                 with_symbol_tex = with_symbol_tex.edit(
-                    span.start, span.end, f"[[FORMULA]]"
+                    span.start, span.end, "[[FORMULA]]"
                 )
 
             # Replace each equation with a bag of the symbols that it contains.
@@ -179,6 +179,7 @@ class EmbellishSentences(ArxivBatchCommand[Task, EmbellishedSentence]):
             text_journal=sentence.text_journal,
             sanitized=sentence.sanitized,
             sanitized_journal=sentence.sanitized_journal,
+            validity_guess=sentence.validity_guess,
             section_name=sentence.section_name,
             in_figure=sentence.in_figure,
             in_table=sentence.in_table,

data-processing/entities/sentences/extractor.py
@@ -172,9 +172,12 @@ class SentenceExtractor(EntityExtractor):
             if itemize_end:
                 in_itemize = False
 
-            # Filter sentences to those for which further text processing would be appropriate.
-            # A number of heuristics are used (commented below).
-            is_valid_sentence = all(
+            # Use heuristics about the surrounding text to determine whether or not this
+            # sentence is valid. These heuristics have a number of limitations, and should be
+            # replaced with more mature rules for detecting whether the sentence is indeed in
+            # names section, the abstract, a figure, a table, etc. See documentation of its
+            # limitations here: https://github.com/allenai/scholar-reader/issues/138#issue-678432430
+            validity_guess = all(
                 [
                     # Sentence should appear in a named section.
                     (not self.from_named_sections_only) or section_name,
@@ -194,9 +197,6 @@ class SentenceExtractor(EntityExtractor):
                     not itemize_end,
                 ]
             )
-
-            if not is_valid_sentence:
-                continue
 
             # Sanitize the text, replacing macros and unwanted TeX with text that will be easier
             # for the text processing algorithms to process.
@@ -257,6 +257,7 @@ class SentenceExtractor(EntityExtractor):
                 sanitized_journal=sanitized,
                 tex=sentence_tex,
                 context_tex=context_tex,
+                validity_guess=validity_guess,
                 section_name=section_name,
                 in_figure=in_figure,
                 in_table=in_table,

data-processing/entities/sentences/types.py
@@ -18,6 +18,12 @@ class Sentence(SerializableEntity):
     TeX, even if they appear in the middle of the string.
     """
 
+    validity_guess: bool
+    """
+    A guess as to whether this sentence is really a text sentence. It's a guess, because the
+    current heuristics used are not always accurate.
+    """
+
     section_name: Optional[str]
     in_figure: bool
     in_table: bool

commit 769f67af56c71e28f1aa5ac31d5b587a2e653fc8

Pipeline: fix up sentence-splitting bug introduced in e7a6f523f834fa60ee90d67901208d0becbd37a8
A regular expression was erroneously written as a non-matching group instead of as a lookahead

data-processing/common/parse_tex.py
@@ -392,7 +392,7 @@ def extract_plaintext(tex_path: str, tex: str) -> JournaledString:
     # to change as little of the original TeX as possible, to make it easier to map
     # back from the original period position (which will often occur at the end of
     # an extracted sentence) to its precise position in the original TeX.
-    for m in reversed(list(re.finditer(r"[\s\.]+(?:\.)", str(plaintext)))):
+    for m in reversed(list(re.finditer(r"[\s\.]+(?=\.)", str(plaintext)))):
         plaintext = plaintext.edit(m.start(), m.end(), "")
 
     return plaintext

data-processing/tests/test_colorize_sentences.py
@@ -14,6 +14,7 @@ def create_sentence(tex: str, start: int) -> Sentence:
         text_journal=None,
         sanitized="<sanitized>",
         sanitized_journal=None,
+        validity_guess=True,
         section_name="<current section>",
         in_figure=False,
         in_table=False,

data-processing/tests/test_parse_tex.py
@@ -49,6 +49,7 @@ def test_extract_plaintext_consolidate_periods_across_groups():
     plaintext = extract_plaintext("main.tex", "\\footnote{Sentence.}. Next sentence.")
     assert plaintext == "Sentence. Next sentence."
 
+
 def test_extract_phrases():
     extractor = PhraseExtractor(["word", "two-token phrase"])
     phrases = list(