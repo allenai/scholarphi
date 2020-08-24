0ee8bd7da086d3f0da2a547f0882411f96f85786
Andrew Head <head.andrewm@gmail.com>
2020-08-24 03:49:13 -0700

    UI: Add "Also see..." for other symbols to the tooltip

--- ui/src/ContextualSymbolGloss.tsx
+++ ui/src/ContextualSymbolGloss.tsx
@@ -1,18 +1,24 @@
+import Accordion from "@material-ui/core/Accordion";
+import AccordionDetails from "@material-ui/core/AccordionDetails";
+import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Button from "@material-ui/core/Button";
 import Table from "@material-ui/core/Table";
 import TableBody from "@material-ui/core/TableBody";
 import TableCell from "@material-ui/core/TableCell";
 import TableRow from "@material-ui/core/TableRow";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import classNames from "classnames";
 import React from "react";
 import { getRemoteLogger } from "./logging";
 import RichText from "./RichText";
+import * as selectors from "./selectors";
 import { Entities } from "./state";
 import {
   Entity,
   Equation,
   isEquation,
   isSentence,
+  isSymbol,
   Relationship,
   Sentence,
   Symbol,
@@ -32,6 +38,9 @@ type DetailLevel = "default" | "nicknames" | "definition" | "everything";
 
 interface State {
   detail: DetailLevel;
+  activeSymbolId: string;
+  formulasOpen: boolean;
+  usagesOpen: boolean;
 }
 
 /**
@@ -40,17 +49,20 @@ interface State {
  * some simple (brittle) replacements to clean up the text.
  */
 function cleanTex(contents: string) {
-  const simpleMacro = (name: string) =>
+  const noArgMacro = (name: string) => new RegExp(`\\\\${name}(?:\{\})?`, "g");
+  const oneArgMacro = (name: string) =>
     new RegExp(`\\\\${name}\\{([^}]*)\\}`, "g");
   return contents
     .replace(/%.*?$/gm, "")
-    .replace(simpleMacro("label"), "")
-    .replace(simpleMacro("texttt"), "$1")
-    .replace(simpleMacro("textit|emph"), "$1")
-    .replace(simpleMacro("footnote"), "")
-    .replace(simpleMacro("\\w*cite\\*?"), "[Citation]")
-    .replace(simpleMacro("(?:eq|c|)ref"), "[Reference]")
-    .replace(simpleMacro("gls(?:pl)?\\*"), (_, arg) => arg.toUpperCase());
+    .replace(oneArgMacro("label"), "")
+    .replace(oneArgMacro("texttt"), "$1")
+    .replace(oneArgMacro("textbf"), "$1")
+    .replace(oneArgMacro("textit|emph"), "$1")
+    .replace(oneArgMacro("footnote"), "")
+    .replace(oneArgMacro("\\w*cite\\w*\\*?"), "[Citation]")
+    .replace(oneArgMacro("(?:eq|c|)ref"), "[Reference]")
+    .replace(oneArgMacro("gls(?:pl)?\\*"), (_, arg) => arg.toUpperCase())
+    .replace(noArgMacro("bfseries"), "");
 }
 
 class ContextualSymbolGloss extends React.PureComponent<Props, State> {
@@ -58,28 +70,84 @@ class ContextualSymbolGloss extends React.PureComponent<Props, State> {
     super(props);
     this.state = {
       detail: "everything",
+      activeSymbolId: props.symbol.id,
+      formulasOpen: false,
+      usagesOpen: false,
     };
+    this.onChangeFormulasOpen = this.onChangeFormulasOpen.bind(this);
+    this.onChangeUsagesOpen = this.onChangeUsagesOpen.bind(this);
+    this.setActiveSymbolId = this.setActiveSymbolId.bind(this);
+  }
+
+  setActiveSymbolId(id: string) {
+    const { entities } = this.props;
+    if (entities.byId[id] !== undefined && isSymbol(entities.byId[id])) {
+      this.setState({ activeSymbolId: id });
+    }
+  }
+
+  onChangeFormulasOpen(_, expanded: boolean) {
+    this.setState({ formulasOpen: expanded });
+  }
+
+  onChangeUsagesOpen(_, expanded: boolean) {
+    this.setState({ usagesOpen: expanded });
   }
 
   render() {
-    const { symbol, entities } = this.props;
-    const { detail } = this.state;
+    const { entities } = this.props;
+    const { activeSymbolId, detail } = this.state;
+    const symbol = entities.byId[activeSymbolId] as Symbol;
+
+    const { snippets, sentences: snippetSentences } = getSnippets(
+      symbol,
+      entities
+    );
+    const { formulas, equations: formulaEquations } = getDefiningFormulas(
+      symbol,
+      entities
+    );
+
     const {
       tex,
       nicknames,
       definitions,
-      defining_formulas,
       // is_definition,
-      snippets,
     } = symbol.attributes;
     const {
       parent,
       children,
       nickname_sentences,
       definition_sentences,
-      defining_formula_equations,
-      snippet_sentences,
     } = symbol.relationships;
+
+    const related = entities.all
+      .map((id) => entities.byId[id])
+      .filter((e) => e !== undefined)
+      .filter(isSymbol)
+      .filter(
+        (s) =>
+          s.attributes.tex !== null &&
+          s.attributes.tex === symbol.attributes.tex
+      )
+      .map((s) => s.relationships.children.concat(s.relationships.parent))
+      .flat()
+      .filter((r) => r.id !== null)
+      .map((r) => entities.byId[r.id as string])
+      .filter((e) => e !== undefined)
+      .filter(isSymbol)
+      .filter((s) => s.attributes.tex !== null);
+
+    const alsoSee: Symbol[] = [];
+    const alsoSeeTexs: { [tex: string]: boolean } = {};
+    related.forEach((r) => {
+      if (!alsoSeeTexs[r.attributes.tex as string]) {
+        alsoSee.push(r);
+        alsoSeeTexs[r.attributes.tex as string] = true;
+      }
+    });
+
+    const originalSymbol = this.props.symbol;
 
     const groupedNicknames = groupNicknames(
       nicknames,
@@ -89,7 +157,28 @@ class ContextualSymbolGloss extends React.PureComponent<Props, State> {
     const nicknamesOrdered = uiUtils.sortByFrequency(nicknames);
 
     return (
-      <div className="gloss symbol-definition-gloss">
+      <div
+        className={classNames("gloss", "symbol-definition-gloss", {
+          "mode-everything": detail === "everything",
+        })}
+      >
+        {(detail === "definition" || detail === "everything") &&
+          alsoSee.length > 0 && (
+            <div className="gloss__section">
+              <p>
+                Also see{" "}
+                {alsoSee.map((s, i) => (
+                  <>
+                    <SymbolLink
+                      symbol={s}
+                      handleSetActiveSymbol={this.setActiveSymbolId}
+                    />
+                    {i < alsoSee.length - 1 && ", "}
+                  </>
+                ))}
+              </p>
+            </div>
+          )}
         {(detail === "nicknames" || detail === "everything") &&
         nicknames.length > 0 ? (
           <div className="gloss__section">
@@ -118,59 +207,239 @@ class ContextualSymbolGloss extends React.PureComponent<Props, State> {
             </p>
           </div>
         ) : null}
-        {detail === "everything" && defining_formulas.length > 0 ? (
+        {detail === "everything" && formulas.length > 0 ? (
           <div className="gloss__section">
-            <GlossSection>
-              {defining_formulas.map((f, i) => {
-                let equation = undefined;
-                const equationRelationship = defining_formula_equations[i];
-                if (equationRelationship && equationRelationship.id !== null) {
-                  const entity = entities.byId[equationRelationship.id];
-                  if (isEquation(entity)) {
-                    equation = entity;
-                  }
-                }
-                return (
-                  <DefiningFormula
-                    key={i}
-                    equation={equation}
-                    handleJumpToEquation={this.props.handleJumpToEntity}
-                  >
-                    {f}
-                  </DefiningFormula>
-                );
-              })}
-            </GlossSection>
+            <Accordion
+              expanded={this.state.formulasOpen}
+              onChange={this.onChangeFormulasOpen}
+            >
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                Defining Formulas
+              </AccordionSummary>
+              <AccordionDetails>
+                <div>
+                  <p className="gloss-section__label">
+                    Showing defining formulas. Formulas before this page are
+                    shown first.
+                  </p>
+                  <GlossSection>
+                    {formulas.map((f, i) => (
+                      <DefiningFormula
+                        key={i}
+                        equation={formulaEquations[i]}
+                        linkText={getLinkText(
+                          originalSymbol,
+                          formulaEquations[i]
+                        )}
+                        handleJumpToEquation={this.props.handleJumpToEntity}
+                      >
+                        {f}
+                      </DefiningFormula>
+                    ))}
+                  </GlossSection>
+                </div>
+              </AccordionDetails>
+            </Accordion>
           </div>
         ) : null}
         {detail === "everything" && snippets.length > 0 ? (
           <div className="gloss__section">
-            <GlossSection>
-              {snippets.map((s, i) => {
-                let sentence = undefined;
-                const sentenceRelationship = snippet_sentences[i];
-                if (sentenceRelationship && sentenceRelationship.id !== null) {
-                  const entity = entities.byId[sentenceRelationship.id];
-                  if (isSentence(entity)) {
-                    sentence = entity;
-                  }
-                }
-                return (
-                  <Snippet
-                    key={i}
-                    sentence={sentence}
-                    handleJumpToSnippet={this.props.handleJumpToEntity}
-                  >
-                    {s}
-                  </Snippet>
-                );
-              })}
-            </GlossSection>
+            <Accordion
+              expanded={this.state.usagesOpen}
+              onChange={this.onChangeUsagesOpen}
+            >
+              <AccordionSummary expandIcon={<ExpandMoreIcon />}>
+                Sample Usages
+              </AccordionSummary>
+              <AccordionDetails>
+                <div>
+                  <p className="gloss-section__label">
+                    Showing usages of this symbol. Usages after this page are
+                    shown first.
+                  </p>
+                  <GlossSection>
+                    {snippets.map((s, i) => (
+                      <Snippet
+                        key={i}
+                        sentence={snippetSentences[i]}
+                        linkText={getLinkText(
+                          originalSymbol,
+                          snippetSentences[i]
+                        )}
+                        handleJumpToSnippet={this.props.handleJumpToEntity}
+                      >
+                        {s}
+                      </Snippet>
+                    ))}
+                  </GlossSection>
+                </div>
+              </AccordionDetails>
+            </Accordion>
           </div>
         ) : null}
       </div>
     );
   }
+}
+
+function getLinkText(source: Entity, destination: Entity) {
+  const sourcePage = getFirstPage(source);
+  const destPage = getFirstPage(destination);
+  if (sourcePage === null || destPage === null) {
+    return null;
+  }
+  if (sourcePage === destPage) {
+    return `See in context on this page`;
+  } else {
+    return `See in context on page ${destPage + 1}`;
+  }
+}
+
+function getDefiningFormulas(symbol: Symbol, entities: Entities) {
+  const currentPage = getFirstPage(symbol);
+  if (currentPage === null) {
+    return { formulas: [], equations: [] };
+  }
+
+  const { defining_formulas } = symbol.attributes;
+  const { defining_formula_equations } = symbol.relationships;
+
+  const before = [];
+  const onPage = [];
+  const after = [];
+
+  for (let i = 0; i < defining_formulas.length; i++) {
+    const formula = defining_formulas[i];
+    const equationRelationship = defining_formula_equations[i];
+    if (
+      equationRelationship === undefined ||
+      equationRelationship.id === null
+    ) {
+      continue;
+    }
+    const equation = entities.byId[equationRelationship.id];
+    if (equation === undefined || !isEquation(equation)) {
+      continue;
+    }
+    const equationPage = getFirstPage(equation);
+    if (equationPage === null) {
+      continue;
+    }
+    if (equationPage === currentPage) {
+      onPage.push({ formula, equation });
+    }
+    if (equationPage < currentPage) {
+      before.push({ formula, equation });
+    }
+    if (equationPage > currentPage) {
+      after.push({ formula, equation });
+    }
+  }
+
+  before.sort((f1, f2) => {
+    return comparePosition(f1.equation, f2.equation);
+  });
+  onPage.sort((f1, f2) => {
+    return comparePosition(f1.equation, f2.equation);
+  });
+  after.sort((f1, f2) => {
+    return comparePosition(f1.equation, f2.equation);
+  });
+
+  return {
+    formulas: [
+      before.map((f) => f.formula),
+      after.map((f) => f.formula),
+      onPage.map((f) => f.formula),
+    ].flat(),
+    equations: [
+      before.map((f) => f.equation),
+      after.map((f) => f.equation),
+      onPage.map((f) => f.equation),
+    ].flat(),
+  };
+}
+
+function comparePosition(e1: Entity, e2: Entity) {
+  if (
+    e1.attributes.bounding_boxes.length === 0 ||
+    e2.attributes.bounding_boxes.length === 0
+  ) {
+    return 0;
+  }
+  return selectors.compareBoxes(
+    e1.attributes.bounding_boxes[0],
+    e2.attributes.bounding_boxes[0]
+  );
+}
+
+function getSnippets(symbol: Symbol, entities: Entities) {
+  const currentPage = getFirstPage(symbol);
+  if (currentPage === null) {
+    return { snippets: [], sentences: [] };
+  }
+
+  const { snippets } = symbol.attributes;
+  const { snippet_sentences } = symbol.relationships;
+
+  const before = [];
+  const onPage = [];
+  const after = [];
+
+  for (let i = 0; i < snippets.length; i++) {
+    const snippet = snippets[i];
+    const MAXIMUM_SNIPPET_LENGTH = 200;
+    if (snippet.length > MAXIMUM_SNIPPET_LENGTH) {
+      continue;
+    }
+    const sentenceRelationship = snippet_sentences[i];
+    if (
+      sentenceRelationship === undefined ||
+      sentenceRelationship.id === null
+    ) {
+      continue;
+    }
+    const sentence = entities.byId[sentenceRelationship.id];
+    if (sentence === undefined || !isSentence(sentence)) {
+      continue;
+    }
+    const sentencePage = getFirstPage(sentence);
+    if (sentencePage === null) {
+      continue;
+    }
+    if (sentencePage === currentPage) {
+      onPage.push({ snippet, sentence });
+    }
+    if (sentencePage < currentPage) {
+      before.push({ snippet, sentence });
+    }
+    if (sentencePage > currentPage) {
+      after.push({ snippet, sentence });
+    }
+  }
+
+  before.sort((s1, s2) => {
+    return comparePosition(s1.sentence, s2.sentence);
+  });
+  onPage.sort((s1, s2) => {
+    return comparePosition(s1.sentence, s2.sentence);
+  });
+  after.sort((s1, s2) => {
+    return comparePosition(s1.sentence, s2.sentence);
+  });
+
+  return {
+    snippets: [
+      after.map((s) => s.snippet),
+      before.map((s) => s.snippet),
+      onPage.map((s) => s.snippet),
+    ].flat(),
+    sentences: [
+      after.map((s) => s.sentence),
+      before.map((s) => s.sentence),
+      onPage.map((s) => s.sentence),
+    ].flat(),
+  };
 }
 
 function groupNicknames(
@@ -210,6 +479,33 @@ function groupNicknames(
   return grouped;
 }
 
+interface SymbolLinkProps {
+  symbol: Symbol;
+  handleSetActiveSymbol: (symbolId: string) => void;
+}
+
+class SymbolLink extends React.PureComponent<SymbolLinkProps> {
+  constructor(props: SymbolLinkProps) {
+    super(props);
+    this.onClick = this.onClick.bind(this);
+  }
+
+  onClick() {
+    this.props.handleSetActiveSymbol(this.props.symbol.id);
+  }
+
+  render() {
+    if (this.props.symbol.attributes.tex === null) {
+      return null;
+    }
+    return (
+      <span className="other-symbol-link" onClick={this.onClick}>
+        <RichText>{this.props.symbol.attributes.tex as string}</RichText>
+      </span>
+    );
+  }
+}
+
 interface EntityLinkSpanProps {
   className?: string;
   entityId?: string | null;
@@ -244,6 +540,7 @@ class EntityLinkSpan extends React.PureComponent<EntityLinkSpanProps> {
 
 interface DefiningFormulaProps {
   equation?: Equation;
+  linkText?: string | null;
   handleJumpToEquation?: (equationId: string) => void;
 }
 
@@ -281,11 +578,7 @@ class DefiningFormula extends React.PureComponent<DefiningFormulaProps> {
   }
 
   render() {
-    let equationPage = null;
     const { equation } = this.props;
-    if (equation) {
-      equationPage = getFirstPage(equation);
-    }
     return (
       <>
         <div
@@ -297,12 +590,14 @@ class DefiningFormula extends React.PureComponent<DefiningFormulaProps> {
         >
           <RichText>{`\$${this.props.children}\$`}</RichText>
         </div>
-        {this.props.handleJumpToEquation && equation && equationPage && (
+        {this.props.handleJumpToEquation && equation && this.props.linkText && (
           <p>
             <EntityLinkSpan
               entityId={equation.id}
               handleJumpToEntity={this.props.handleJumpToEquation}
-            >{`See in context on page ${equationPage}`}</EntityLinkSpan>
+            >
+              {this.props.linkText}
+            </EntityLinkSpan>
             .
           </p>
         )}
@@ -315,33 +610,32 @@ class DefiningFormula extends React.PureComponent<DefiningFormulaProps> {
 
 interface SnippetProps {
   sentence?: Sentence;
+  linkText?: string | null;
   handleJumpToSnippet?: (sentenceId: string) => void;
   children: string;
 }
 
 class Snippet extends React.PureComponent<SnippetProps> {
   render() {
-    let sentencePage = null;
     const { sentence } = this.props;
-    if (sentence) {
-      sentencePage = getFirstPage(sentence);
-    }
     return (
-      <div className="snippet">
-        <RichText>{cleanTex(this.props.children)}</RichText>
-        {this.props.handleJumpToSnippet && sentencePage ? (
-          <>
+      <>
+        <div className="snippet">
+          <RichText>{cleanTex(this.props.children)}</RichText>
+        </div>
+        {this.props.handleJumpToSnippet && this.props.linkText ? (
+          <p>
             {" "}
             <EntityLinkSpan
               entityId={sentence ? sentence.id : undefined}
               handleJumpToEntity={this.props.handleJumpToSnippet}
             >
-              {`See in context on page ${sentencePage}`}
+              {this.props.linkText}
             </EntityLinkSpan>
             {"."}
-          </>
+          </p>
         ) : null}
-      </div>
+      </>
     );
   }
 }
