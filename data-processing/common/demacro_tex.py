b16fcfdc160f9b4bbffa17c772c12a2e111aaf99
kyleclo <kyleclo@uw.edu>
2021-01-03 20:43:32 -0800

    skip comment lines when demacroing

--- data-processing/common/demacro_tex.py
+++ data-processing/common/demacro_tex.py
@@ -10,15 +10,32 @@ from common.scan_tex import Pattern, scan_tex
 from common.types import CharacterRange, Path
 
 
-prefix_patterns = [
-    Pattern(name='newcommand', regex=r'\\newcommand(.*?)\{(\\.+?)\}(.*?\n)'),
-    Pattern(name='renewcommand', regex=r'\\renewcommand(.*?)\{(\\.+?)\}(.*?\n)'),
-    Pattern(name='declaremathoperator', regex=r'\\DeclareMathOperator(.*?)\{(\\.+?)\}(.*?\n)')
-]
+def is_line_comment(line: str) -> bool:
+    for char in line:
+        if char.strip() == '':
+            continue
+        elif char == '%':
+            return True
+        else:
+            return False
+
 
 def locate_macro_definitions(tex: str) -> List[Tuple]:
-    matches = list(scan_tex(tex=tex, patterns=prefix_patterns))
-    return [(match.start, match.end) for match in matches]
+    prefix_patterns = [
+        Pattern(name='newcommand', regex=r'\\newcommand(.*?)\{(\\.+?)\}'),
+        Pattern(name='renewcommand', regex=r'\\renewcommand(.*?)\{(\\.+?)\}'),
+        Pattern(name='declaremathoperator', regex=r'\\DeclareMathOperator(.*?)\{(\\.+?)\}')
+    ]
+    spans = []
+    for i, line in enumerate(tex.split('\n')):
+        # skip any lines that are comments
+        if is_line_comment(line):
+            continue
+        else:
+            matches = list(scan_tex(tex=line, patterns=prefix_patterns))
+            for match in matches:
+                spans.append((i, match.start, match.end))
+    return spans
 
 
 def demacro_tex(tex_filepath: Path) -> Optional[str]:
--- data-processing/tests/test_demacro_tex.py
+++ data-processing/tests/test_demacro_tex.py
@@ -30,11 +30,26 @@ def test_locate_macro_definitions_simple():
     ]
     tex = "\n".join(tex_lines)
     spans = locate_macro_definitions(tex=tex)
-    assert tex[spans[0][0]:spans[0][1]] == "\\newcommand{\\R}{{\\mathbb R}}\n"
-    assert tex[spans[1][0]:spans[1][1]] == "\\renewcommand{\\vector}[1]{(x_1,x_2,\\ldots,x_{#1})}\n"
-    assert tex[spans[2][0]:spans[2][1]] == "\\newcommand{\\avector}[2]{(#1_1,#1_2,\\ldots,#1_{#2})}\n"
-    assert tex[spans[3][0]:spans[3][1]] == "\\DeclareMathOperator*{\\minimize}{minimize}\n"
+    assert tex_lines[spans[0][0]][spans[0][1]:spans[0][2]] == "\\newcommand{\\R}"
+    assert tex_lines[spans[1][0]][spans[1][1]:spans[1][2]] == "\\renewcommand{\\vector}"
+    assert tex_lines[spans[2][0]][spans[2][1]:spans[2][2]] == "\\newcommand{\\avector}"
+    assert tex_lines[spans[3][0]][spans[3][1]:spans[3][2]] == "\\DeclareMathOperator*{\\minimize}"
 
+
+def test_locate_macro_definitions_skip_commented():
+    tex_lines = [
+        "\\documentclass{article}",
+        "% \\newcommand{\\R}{{\\mathbb R}}",
+        "% \\renewcommand{\\vector}[1]{(x_1,x_2,\\ldots,x_{#1})}",
+        "%\\newcommand{\\avector}[2]{(#1_1,#1_2,\\ldots,#1_{#2})}",
+        "%\\DeclareMathOperator*{\\minimize}{minimize}",
+        "\\begin{document}",
+        "Body text",
+        "\\end{document}",
+    ]
+    tex = "\n".join(tex_lines)
+    spans = locate_macro_definitions(tex=tex)
+    assert len(spans) == 0
 
 
 def test_locate_macro_definitions_multiple_on_single_line():
