075ce3dfa4d28061441396ba7bb8b5b73434b197
kyleclo <kyleclo@uw.edu>
2021-01-22 00:46:23 -0800

    organize methods more; add clustering tokens into rows

--- data-processing/play_with_zejiang_boxes.py
+++ data-processing/play_with_zejiang_boxes.py
@@ -47,23 +47,52 @@ class TokenWithBBox:
         return self.__str__()
 
 
+class SentenceWithBbox:
+    def __init__(self, text: str, bboxes: List[BoundingBox], tokens: List[TokenWithBBox]):
+        self.text = text
+        self.bboxes = bboxes
+        self.tokens = tokens
+
+    def __str__(self):
+        s = []
+        for bbox in self.bboxes:
+            s.append(f"(left={bbox.left:.2f}, top={bbox.top:.2f}, width={bbox.width:.2f}, height={bbox.height:.2f}, page={bbox.page})")
+        return f"{self.text}\t" + '\t'.join(s)
+
+    def __repr__(self):
+        return self.__str__()
+
+
 class BlockWithBbox:
     def __init__(self, tokens: List[TokenWithBBox], bbox: BoundingBox, type: str):
         self.tokens = tokens
         self.bbox = bbox
         self.type = type
 
-        self.text = ' '.join([t.text for t in self.tokens])
-        self.token_char_spans = self.compute_token_char_spans()
-        self.sent_char_spans = self.compute_sent_char_spans()
-        self.sents = [self.text[start:end] for start, end in self.sent_char_spans]
+        # 1) compute what the entire text block looks like & use it to represent tokens & sentences in a shared char index space
+        self.text: str = ' '.join([t.text for t in self.tokens])
+        self.token_char_spans: List[Tuple] = self._compute_token_char_spans()
+        self.sent_char_spans: List[Tuple] = self._compute_sent_char_spans()
 
-        # [[0, 1, 2], [3, 4, 5], [6, 7, 8]] means the first 3 tokens (0, 1, 2) belong in sentence 0, next 3 in sentence 1, etc.
-        self.token_sent_clusters = self.cluster_tokens_by_sents()
+        # 2) use these spans to align tokens to sentences
+        #       [[0, 1, 2], [3, 4, 5], ...] means the first 3 tokens (0, 1, 2) belong in sentence 0, next 3 in sentence 1, etc.
+        self.token_sent_clusters: List[List[int]] = self._cluster_tokens_by_sents()
 
-        
+        # 3) cluster tokens into rows
+        #       [[0, 1, 2], [3, 4, 5], ...] means the first 3 tokens (0, 1, 2) belong in row 0, next 3 in row 1, etc.
+        self.token_row_clusters: List[List[int]] = self._cluster_tokens_by_row()
 
-    def compute_sent_char_spans(self) -> List[Tuple]:
+        # 4) using the rows, compute bboxes for sentences
+
+
+        # self.sents: List[str] = [self.text[start:end] for start, end in self.sent_char_spans]
+
+
+    #
+    #   Methods for step (1)
+    #
+
+    def _compute_sent_char_spans(self) -> List[Tuple]:
         sent_char_spans = []
         start = 0
         doc = nlp(self.text)
@@ -74,8 +103,8 @@ class BlockWithBbox:
                 start = doc[i].idx
         return sent_char_spans
 
-    def compute_token_char_spans(self) -> List[Tuple]:
-        # TODO; this only works because all tokens are whitespace delim  (hence only +1)
+    def _compute_token_char_spans(self) -> List[Tuple]:
+        # TODO; this only works because all tokens are whitespace delim  (hence start = end + 1)
         token_char_spans = []
         start = 0
         for t in self.tokens:
@@ -83,6 +112,10 @@ class BlockWithBbox:
             token_char_spans.append((start, end))
             start = end + 1
         return token_char_spans
+
+    #
+    #   Methods for step (2)
+    #
 
     def _find_sent_idx_for_this_token_char_span(self, token_start: int, token_end: int) -> int:
         # quantifies how much of token "sticks out" of the sentence boundary, for each sentence
@@ -98,13 +131,37 @@ class BlockWithBbox:
         idx_best_sent = np.argmin(leftovers_per_sent)
         return int(idx_best_sent)
 
-    def cluster_tokens_by_sents(self) -> List[List[int]]:
+    def _cluster_tokens_by_sents(self) -> List[List[int]]:
         sent_clustered_token_idx = [[] for _ in range(len(self.sents))]
         for token_idx, (token_start, token_end) in enumerate(self.token_char_spans):
             sent_idx = self._find_sent_idx_for_this_token_char_span(token_start=token_start, token_end=token_end)
             sent_clustered_token_idx[sent_idx].append(token_idx)
         return sent_clustered_token_idx
 
+
+    #
+    #   Methods for step (3)
+    #
+
+    # TODO: ther'es probably a better way to solve this clustering problem as an optim problem.
+    #       for example, dbscan or k-means w/ distance metric based on y-coord
+    def _cluster_tokens_by_row(self) -> List[List[int]]:
+        # cluster incrementally O(num_tokens)
+        clusters = [[0]]
+        for token_idx in range(1, len(self.tokens)):
+            current_token = self.tokens[token_idx]
+            prev_token = self.tokens[token_idx - 1]
+            if _are_same_row(bbox1=prev_token.bbox, bbox2=current_token.bbox):
+                clusters[-1].append(token_idx)
+            else:
+                clusters.append([token_idx])
+        return clusters
+
+
+    #
+    #   Methods for step (4)
+    #
+    
 
 def normalize_spp_bbox_json(spp_bbox_json: Dict, page_id: int, spp_page_height: float, spp_page_width: float) -> BoundingBox:
     left, top, right, bottom = spp_bbox_json
@@ -114,6 +171,24 @@ def normalize_spp_bbox_json(spp_bbox_json: Dict, page_id: int, spp_page_height: 
                        width=(spp_bbox.right - spp_bbox.left) / spp_page_width,
                        height=(spp_bbox.bottom - spp_bbox.top) / spp_page_height,
                        page=spp_bbox.page_id)
+
+
+def _are_same_row(bbox1: BoundingBox, bbox2: BoundingBox) -> bool:
+    """Taken from https://github.com/allenai/scholarphi/blob/381eea6cf540fdc130fc10f032b883fa0ef64af4/data-processing/pdf/process_pdf.py#L61"""
+    return bbox1.page == bbox2.page and (
+        abs(bbox1.top - bbox2.top) < 0.01  # Same y-coordinate
+    ) #and (
+        # abs(bbox2.left - bbox1.left - bbox1.width) < 0.05     # To the right
+    # )
+
+
+def _union_bboxes(bbox1: BoundingBox, bbox2: BoundingBox) -> BoundingBox:
+    """Taken from https://github.com/allenai/scholarphi/blob/381eea6cf540fdc130fc10f032b883fa0ef64af4/data-processing/pdf/process_pdf.py#L48"""
+    x1 = min(bbox1.left, bbox2.left)
+    y1 = min(bbox1.top, bbox2.top)
+    x2 = max(bbox1.left + bbox1.width, bbox2.left + bbox2.width)
+    y2 = max(bbox1.top + bbox1.height, bbox2.top + bbox2.height)
+    return BoundingBox(page = bbox1.page, left = x1, top = y1, width = x2 - x1, height=y2 - y1)
 
 
 
@@ -129,7 +204,6 @@ def process_page(page_dict: dict) -> dict:
                                                           page_id=page_id,
                                                           spp_page_height=page_height,
                                                           spp_page_width=page_width)
-        block_type = block_dict['type']
         block_tokens: List[TokenWithBBox] = [
             TokenWithBBox(
                 text=token_dict['text'],
@@ -141,8 +215,7 @@ def process_page(page_dict: dict) -> dict:
             for token_dict in block_dict['tokens']
         ]
 
-
-        block = BlockWithBbox(tokens=block_tokens, bbox=block_bbox, type=block_type)
+        block = BlockWithBbox(tokens=block_tokens, bbox=block_bbox, type=block_dict['type'])
 
         # TODO: stopped here -- double-checking that the token clusters form proper sentences
         for sent_idx, token_sent_cluster in enumerate(block.token_sent_clusters):
@@ -163,3 +236,5 @@ for page_dict in tqdm(page_dicts):
 
 for paragraph in paragraphs:
     print(paragraph)
+
+
