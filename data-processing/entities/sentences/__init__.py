33750fe4afe53cfdd46a2f2609f5420c0074f620
kyleclo <kyleclo@uw.edu>
2021-03-15 12:55:53 -0700

    add command to fetch SPP JSON from service; register it

--- data-processing/entities/sentences/__init__.py
+++ data-processing/entities/sentences/__init__.py
@@ -4,6 +4,8 @@ from common import directories
 from entities.common import create_entity_localization_command_sequence
 from common.commands.detect_entities import make_detect_entities_command
 from common.commands.upload_entities import make_upload_entities_command
+from entities.sentences.commands.locate_sentences import LocateSentencesCommand
+from entities.sentences.commands.fetch_spp_jsons import FetchSppJsons
 from scripts.pipelines import EntityPipeline, register_entity_pipeline
 
 from .colorize import adjust_color_positions
@@ -11,19 +13,6 @@ from .extractor import SentenceExtractor
 from .types import Sentence
 from .upload import upload_sentences
 
-
-
-# kyle -- remove this
-# commands = create_entity_localization_command_sequence(
-#     "sentences",
-#     SentenceExtractor,
-#     DetectedEntityType=Sentence,
-#     colorize_options=ColorizeOptions(adjust_color_positions=adjust_color_positions),
-#     upload_func=upload_sentences,
-# )
-
-
-# TODO -- manually create steps 1 & 3 instead of using this
 
 commands: CommandList = []
 
@@ -33,11 +22,11 @@ commands.append(make_detect_entities_command('sentences', SentenceExtractor))
 
 # step 2: download spp from s3
 directories.register('fetched-spp-jsons')
-# TODO: missing command
+commands.append(FetchSppJsons)
 
 # step 3: compute sentence bboxes via fuzzy matching
 directories.register(f"sentences-locations")
-# TODO: missing command
+commands.append(LocateSentencesCommand)
 
 # step 4: upload
 upload_command = make_upload_entities_command('sentences', upload_sentences, DetectedEntityType=Sentence)
---
+++ data-processing/entities/sentences/commands/fetch_spp_jsons.py
@@ -0,0 +1,74 @@
+import os
+
+import json
+import time
+from argparse import ArgumentParser
+import logging
+from typing import Iterator, Optional
+from dataclasses import dataclass
+
+from common import file_utils
+from common import directories
+from common.commands.base import ArxivBatchCommand
+from common.fetch_arxiv import FetchFromArxivException, fetch_from_arxiv, fetch_from_s3
+from common.types import ArxivId, RelativePath
+
+
+import requests
+
+#  TODO - dont need
+# DEFAULT_S3_ARXIV_SOURCES_BUCKET = "s2-arxiv-sources"
+
+"""Constants for dealing with retries and delays between call attempts"""
+DEFAULT_FETCH_DELAY = 10  # seconds
+BACKOFF_FETCH_DELAY = 60  # seconds
+MAX_FETCH_ATTEMPTS = 3
+
+
+logger = logging.getLogger(__name__)
+
+
+@dataclass(frozen=True)
+class SppJsonTask:
+    arxiv_id: ArxivId
+    pdf_path: RelativePath
+    spp_host: str
+
+
+class FetchSppJsons(ArxivBatchCommand[ArxivId, None]):
+    @staticmethod
+    def get_name() -> str:
+        return "fetch-spp-jsons"
+
+    @staticmethod
+    def get_description() -> str:
+        return "Send a PDF to SPP to get back a JSON"
+
+    def get_arxiv_ids_dirkey(self) -> Optional[str]:
+        return 'arxiv-pdfs'
+
+    def load(self) -> Iterator[SppJsonTask]:
+        for arxiv_id in self.arxiv_ids:
+            yield SppJsonTask(
+                arxiv_id=arxiv_id,
+                pdf_path=os.path.join(directories.arxiv_subdir('arxiv-pdfs', arxiv_id=arxiv_id), f'{arxiv_id}.pdf'),
+                # TODO -- change to real one
+                spp_host='http://localhost:8080/detect'
+            )
+
+    def process(self, item: SppJsonTask) -> Iterator[dict]:
+        f = open(item.pdf_path, 'rb')
+        files = {"pdf_file": (f.name, f, "multipart/form-data")}
+        r = requests.post(item.spp_host, files=files)
+        spp_json = r.json()
+        yield spp_json
+
+
+    def save(self, item: SppJsonTask, result: dict) -> None:
+        results_dir = directories.arxiv_subdir('fetched-spp-jsons', item.arxiv_id)
+        if not os.path.exists(results_dir):
+            os.makedirs(results_dir)
+        spp_json_path = os.path.join(results_dir, f"{item.arxiv_id}.json")
+        with open(spp_json_path, 'w') as f_out:
+            json.dump(result, f_out)
+
