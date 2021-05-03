946088ea1ddf57946200bff58667cae655483767
Raymond Fok <rayfok@umich.edu>
2021-05-02 20:15:33 -0700

    changing back to dev default schema

--- api/src/index.ts
+++ api/src/index.ts
@@ -5,19 +5,11 @@ import { Config } from "./conf";
 nconf
   .argv()
   .env()
-  .overrides({
-    database: {
-      port: 5432,
-      schema: "dev",
-      host: "scholar-reader.c5tvjmptvzlz.us-west-2.rds.amazonaws.com",
-      database: "scholar-reader",
-    },
-  })
   .file({ file: process.env.SECRETS_FILE || "config/secret.json" })
   .defaults({
     database: {
       port: 5432,
-      schema: "public",
+      schema: "dev",
     },
   });
 
