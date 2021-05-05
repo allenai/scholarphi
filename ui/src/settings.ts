4b5e6877464921eb0813e75619f0b9a967a4ab2a
Andrew Head <head.andrewm@gmail.com>
2021-05-05 14:21:17 -0700

    UI: show underlines for all top-level symbols (not just those with definitions) (#285)

--- ui/src/settings.ts
+++ ui/src/settings.ts
@@ -153,7 +153,7 @@ export function getSettings(presets?: string[]) {
     citationGlossesEnabled: true,
     definitionsInSymbolGloss: false,
     termGlossesEnabled: false,
-    symbolUnderlineMethod: "defined-symbols",
+    symbolUnderlineMethod: "top-level-symbols",
     symbolSearchEnabled: true,
     declutterEnabled: true,
     definitionPreviewEnabled: false,
