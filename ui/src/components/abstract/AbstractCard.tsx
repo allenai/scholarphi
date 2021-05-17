69090277f36a8a5bb4dfbed9953f425b683529d9
Raymond Fok <rayfok@umich.edu>
2021-05-16 22:01:08 -0700

    add no-op chips for color mapping discourse types in abstract card

--- ui/src/components/abstract/AbstractCard.tsx
+++ ui/src/components/abstract/AbstractCard.tsx
@@ -1,5 +1,6 @@
 import Accordion from "@material-ui/core/Accordion";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
+import Chip from "@material-ui/core/Chip";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import classNames from "classnames";
@@ -36,11 +37,12 @@ class AbstractCard extends React.PureComponent<Props> {
       .map((e) => e as Sentence);
 
     const discourse2ColorMap: { [discourse: string]: string } = {
-      Objective: "#9AC2C552",
-      Background: "#C2C6A752",
-      Method: "#ECCE8E52",
-      Result: "#27072252",
+      Objective: "#9AC2C5",
+      Background: "#C2C6A7",
+      Method: "#ECCE8E",
+      Result: "#270722",
     };
+    const opacity = "4D";
 
     const abstractColors = abstractIds
       .map((id) => abstractDiscourseClassification[id])
@@ -55,6 +57,17 @@ class AbstractCard extends React.PureComponent<Props> {
               Abstract
             </Typography>
           </AccordionSummary>
+          <div className={"discourse-chip-palette"}>
+            {Object.entries(discourse2ColorMap).map(([d, color]) => (
+              <Chip
+                className={"discourse-chip"}
+                label={d}
+                onClick={() => {}}
+                style={{ backgroundColor: color + opacity }}
+                variant="outlined"
+              />
+            ))}
+          </div>
           {abstractSentences.map((s, i) =>
             s.attributes.tex !== null ? (
               <div
@@ -66,9 +79,11 @@ class AbstractCard extends React.PureComponent<Props> {
                 onClick={() => this.props.setSelectedAbstractId(s.id)}
               >
                 <div
-                  className={classNames("discourse-tag")}
+                  className={classNames(
+                    `discourse-tag ${abstractDiscourseClassification[s.id]}`
+                  )}
                   style={{
-                    backgroundColor: abstractColors[i],
+                    backgroundColor: abstractColors[i] + opacity,
                   }}
                 ></div>
                 <LatexPreview>
