bd92df27436e020040c86453c1976d6080209e9f
Raymond Fok <rayfok@umich.edu>
2021-09-10 14:41:15 -0700

    update data

--- ui/src/ScholarReader.tsx
+++ ui/src/ScholarReader.tsx
@@ -860,8 +860,13 @@ export default class ScholarReader extends React.PureComponent<Props, State> {
         (r: RhetoricUnit) => r.label === "Method" || r.is_in_expected_section
       )
       .filter((r: RhetoricUnit) => {
-        if (["Objective", "Contribution", "Result"].includes(r.label)) {
+        if (["Objective", "Contribution"].includes(r.label)) {
           return r.is_author_statement;
+        } else if (r.label === "Result") {
+          return (
+            r.is_author_statement ||
+            r.text.toLowerCase().includes("participant")
+          );
         } else {
           return true;
         }
