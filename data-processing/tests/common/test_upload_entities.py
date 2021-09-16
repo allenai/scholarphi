22078613c2a5e694540c08babaabe88ed139b6c7
Chloe Anastasiades <chloea@ip-172-16-36-137.us-west-2.compute.internal>
2021-09-15 17:40:01 -0700

    more upload entities tests

--- data-processing/tests/common/test_upload_entities.py
+++ data-processing/tests/common/test_upload_entities.py
@@ -20,3 +20,39 @@ def test_save_entities_with_file():
 
             assert not mock_upload_entities.called
             assert mock_upload_entities.mock_write_to_file
+
+
+def test_save_entities_with_db():
+    output_details = OutputDetails("db", None)
+
+    with patch("common.upload_entities.upload_entities") as mock_upload_entities:
+        with patch("common.upload_entities.write_to_file") as mock_write_to_file:
+            save_entities(
+                s2_id="s2id",
+                arxiv_id="arxivid",
+                entity_infos=[],
+                data_version=None,
+                output_details=output_details,
+                filename="something.jsonl"
+            )
+
+            assert mock_upload_entities.called
+            assert not mock_upload_entities.mock_write_to_file
+
+
+def test_save_entities_with_both():
+    output_details = OutputDetails("both", "bye")
+
+    with patch("common.upload_entities.upload_entities") as mock_upload_entities:
+        with patch("common.upload_entities.write_to_file") as mock_write_to_file:
+            save_entities(
+                s2_id="s2id",
+                arxiv_id="arxivid",
+                entity_infos=[],
+                data_version=None,
+                output_details=output_details,
+                filename="something.jsonl"
+            )
+
+            assert mock_upload_entities.called
+            assert mock_upload_entities.mock_write_to_file
