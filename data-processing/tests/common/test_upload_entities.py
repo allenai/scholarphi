434e6478afd341199e5a67db08502abbbc6e16a5
Chloe Anastasiades <chloea@ip-172-16-36-137.us-west-2.compute.internal>
2021-09-15 17:37:33 -0700

    one upload entities test

---
+++ data-processing/tests/common/test_upload_entities.py
@@ -0,0 +1,22 @@
+from unittest.mock import Mock, patch
+
+from common.commands.database import OutputDetails
+from common.upload_entities import save_entities
+
+
+def test_save_entities_with_file():
+    output_details = OutputDetails("file", "hi")
+
+    with patch("common.upload_entities.upload_entities") as mock_upload_entities:
+        with patch("common.upload_entities.write_to_file") as mock_write_to_file:
+            save_entities(
+                s2_id="s2id",
+                arxiv_id="arxivid",
+                entity_infos=[],
+                data_version=None,
+                output_details=output_details,
+                filename="something.jsonl"
+            )
+
+            assert not mock_upload_entities.called
+            assert mock_upload_entities.mock_write_to_file
