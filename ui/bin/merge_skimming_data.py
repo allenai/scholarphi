7368b7db54d509a616060a044c7b214ae4e44612
Raymond Fok <rayfok@umich.edu>
2021-09-29 01:17:42 -0700

    add unique id to each detected unit

--- ui/bin/merge_skimming_data.py
+++ ui/bin/merge_skimming_data.py
@@ -9,6 +9,7 @@ input_dirs = [
     "src/data/abstract",
 ]
 
+cur_id = 0
 for input_dir in input_dirs:
     merged = {}
     for data_e in os.scandir(input_dir):
@@ -20,7 +21,12 @@ for input_dir in input_dirs:
             continue
 
         with open(data_e.path, "r") as f:
-            merged[id] = json.load(f)
+            data = json.load(f)
+            # add unique id to each data entry
+            for x in data:
+                x["id"] = str(cur_id)
+                cur_id += 1
+            merged[id] = data
 
     with open(f"{input_dir}/skimmingData.json", "w") as out:
         json.dump(merged, out, indent=2)
