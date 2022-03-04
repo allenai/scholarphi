507636bf32fb342539e0bd0cd879bd48d50a80ae
Fangzhou Hu <fhu6@wisc.edu>
2022-03-04 09:32:26 -0800

    Move fetch pdf from tex prepapreation pipline to sentence pipeline

--- data-processing/entities/sentences/__init__.py
+++ data-processing/entities/sentences/__init__.py
@@ -1,6 +1,7 @@
 from common.commands.base import CommandList
 from common.commands.detect_entities import make_detect_entities_command
 from common.commands.upload_entities import make_upload_entities_command
+from common.commands.fetch_arxiv_pdf import FetchArxivPdf
 from entities.sentences_pdf.commands.locate_sentences import LocateSentencesCommand
 from entities.sentences_pdf.upload import upload_sentences
 from scripts.pipelines import EntityPipeline, register_entity_pipeline
@@ -10,7 +11,7 @@ from .types import Sentence
 
 from common import directories
 
-commands: CommandList = []
+commands: CommandList = [FetchArxivPdf]
 
 # step 1: detect sentences in latex
 directories.register(f"detected-sentences")
