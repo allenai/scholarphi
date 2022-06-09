d90b886d62d6cb556b1a91fee89df690c42a67e1
Egor Klevak <egork@allenai.org>
2022-06-09 11:36:29 -0700

    scholar-32314 Adding a Bibitem which much closer matches the reference item in order to pass the score threshold of 0.2

--- data-processing/tests/entities/citations/commands/test_resolve_bibitems.py
+++ data-processing/tests/entities/citations/commands/test_resolve_bibitems.py
@@ -28,15 +28,22 @@ def test_process():
             bibitems=[Bibitem(id_='one1999two', text='dog cat monkey', context_tex='N/A',
                               start=-1, end=-1, tex_path='N/A',
                               tex='N/A', bibitem_code='\journal{dog and cats} dog cat monkey'),
-                     Bibitem(id_='two2020two', text='one two three', context_tex='N/A', start=-1, end=-1,
-                             tex_path='N/A', tex='N/A', bibitem_code='\journal{numerology} one two three')],
+                      Bibitem(id_='two2020two', text='one two three', context_tex='N/A', start=-1, end=-1,
+                              tex_path='N/A', tex='N/A', bibitem_code='\journal{numerology} one two three'),
+                      Bibitem(id_='three2020', text='Dog on a walk', context_tex='N/A', start=-1, end=-1,
+                              tex_path='N/A', tex='N/A',
+                              bibitem_code=('arxiv_id=2022.04708 venue \journal{journal}{arXiv} Charles Dickens '
+                                            'Ernest Hemingway 2022 dog on a walk')),
+                      ],
 
             references=[SerializableReference(s2_id='111', arxivId='2022.04708', doi=None, title='Dog on a walk',
                                    authors="[{'id': 1, 'name': 'Charles Dickens'}, {'id':2, 'name': 'Ernest Hemingway'}]",
                                    venue='arXiv', year='2022')]
         )
         result = next(resolve_bibitems.process(item))
-        assert result == BibitemMatch(key='one1999two', bibitem_text='dog cat monkey', s2_id='111',
-                                      s2_title='Dog on a walk', bibitem_code='\journal{dog and cats} dog cat monkey',
-                                      similarity_score=0.02564102564102564)
+        assert result == BibitemMatch(key='three2020', bibitem_text='Dog on a walk', s2_id='111',
+                                      s2_title='Dog on a walk',
+                                      bibitem_code=('arxiv_id=2022.04708 venue \journal{journal}{arXiv} '
+                                                    'Charles Dickens Ernest Hemingway 2022 dog on a walk'),
+                                      similarity_score=0.4358974358974359)
         assert isinstance(result, BibitemMatch)
